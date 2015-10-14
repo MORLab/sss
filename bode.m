02a5d7c278e586b3e1f33a3b9c462672dee37a5f
Lisa <lisa.jeschek@tum.de>
2015-10-14 22:54:24 +0200

    Update of bode.m and freqresp.m
    • input:
    sss: - only 1 system
           - omega not required -> frequency range calculated
           - plot options: {‘color‘, ‘b‘}
    tax: - more than 1 system possible
           - omega required: frequency range required as input
           - plot options: from bode-ss: e.g. sys1, ‘r--‘, sys2, ‘b-‘,…
    
    -> changes in bode.m
        - input of systems: allow input of more than one system:
        - input of omega: omega given -> freqency range given
                                       omega not given -> frequency range calculated (from sss-bode)
         - plot option: ss-bode plot options
    
    • plot
    sss:    - only 1 system (range either given as omega or calculated)
                - mimo -> subplots
                - plot options: ‘color‘ required (see input)
     tax:    - ss-bode(varargin{:}) -> more than one systems
               - mimo -> not implemented as frd-model?
               - plot options: just ’r--‘,… (see input)
    
    -> changes in bode.m:
         - ss-bode plot: bode(varargin{:}), varargin{:} consists of frd-models (frequency response data model)
         - mimo -> create mimo frd-model
    
    • output
    ss-bode: [mag,phase,wout,sdmag,sdphase] = bode(sys), output not as cell
    
    -> changes in bode.m:
           - output not as cell
           - input: - 1 system -> calculate mag, phase,… from system -> output
                        - more than 1 system: no output
           - sdmag and sdphase are yet missing
    
    • freqresp.m (called from bode.m)
    - ss-freqresp: output not as cell
    - tax: parfor for large systems
    
    -> changes in bode.m
         - call freqresp and then transform to cell
    
    -> changes in freqresp.m
         - output not as cell
         - parfor for large systems

--- bode.m
+++ bode.m
@@ -1,48 +1,159 @@
-function varargout = bode(varargin)
-% Bode plot for sss objects. 
+function  [mag, phase, omega] = bode(varargin)
+% Plots the bode diagram of an LTI system
 % ------------------------------------------------------------------
-% [mag,phase,wout,sdmag,sdphase] = bode(sys1,sys2,...,sysN,omega,PlotStyleN)
-% Inputs:       * sys1: must be sss
-%               * omega = 2*pi*f must be specified
-% Outputs:      * [mag,phase,wout,sdmag,sdphase] see matlab docu
-%               * idfrd object if number of outputs is one
+% [mag, phase, omega] = bode(sys, omega, in, out, options)
+% Inputs:       * sys: an sss-object containing the LTI system
+%    [optional] * vector of imaginary frequencies to plot at
+%               * plot options. see <a href="matlab:help plot">PLOT</a>
+% Outputs:      * vector of complex frequency response values
 % ------------------------------------------------------------------
-% Authors:      Stefan Jaensch, Heiko Panzer
-%
-% see also: freqresp
+% This file is part of the MORLAB_GUI, a Model Order Reduction and
+% System Analysis Toolbox developed at the
+% Institute of Automatic Control, Technische Universitaet Muenchen
+% For updates and further information please visit www.rt.mw.tum.de
+% ------------------------------------------------------------------
+% Authors:      Heiko Panzer (heiko@mytum.de), Sylvia Cremer, Rudy Eid,
+%               Alessandro Castagnotto, Lisa Jeschek
+% Last Change:  14 Oct 2015
+% ------------------------------------------------------------------
 
+
+% --------------- EVALUATE OPTIONS ---------------
 omegaIndex = cellfun(@isfloat,varargin);
-if all(~omegaIndex)
-   error('Frequency vector must be specified for sss/bode') 
+if ~isempty(omegaIndex) && nnz(omegaIndex)
+    omega = varargin{omegaIndex};
+    varargin{omegaIndex}=[];
 end
-omega = varargin{omegaIndex};
 
 for i = 1:length(varargin)
     if isa(varargin{i},'sss')
-        if varargin{i}.Ts == 0
-            G = freqresp(varargin{i},1i* omega);
-        else
-            G = freqresp(varargin{i},exp(1i* omega*varargin{i}.Ts));
-        end        
-        if isempty(varargin{i}.Name)
-             varargin{i}.Name = inputname(i);
-        end        
-        varargin{i} = frd(G,omega,varargin{i}.Ts,...
-            'InputName',varargin{i}.InputName,'OutputName',varargin{i}.OutputName,...
-            'Name',varargin{i}.Name);
-    elseif isa(varargin{i},'numlti') || isa(varargin{i},'idlti')
-        if isempty(varargin{i}.Name)
-             varargin{i}.Name = inputname(i);
-        end    
-    end    
-end
-   
-if nargout== 1
-   varargout{1} =  varargin{1};
-elseif nargout
-    [varargout{1},varargout{2},varargout{3},varargout{4},varargout{5}] = bode(varargin{:});    
-else
-    bode(varargin{:});
+        nSys=i;
+    end
 end
 
+% --------------- CALCULATE FREQUENCY RESPONSE ---------------
+for i = 1:length(varargin)
+    if isa(varargin{i},'sss')
+        if exist('omega', 'var') && ~isempty(omega)
+            % --------- frequency range values given ---------
+            if size(omega,1)>1
+                omega=transpose(omega);
+            end
+            m = freqrespCell(varargin{i}, 1j*abs(omega));
+        else
+            % --------- frequency range needs to be chosen ---------
+            dc = freqrespCell(varargin{i},0);    % G(0)=DCgain
+            ft = freqrespCell(varargin{i},inf);  % G(inf)=feedthrough
+
+            %determine minimum frequency
+            if any(any(cellfun(@isinf,dc))) || any(any(cellfun(@isnan,dc))) % pole at s=0
+                wmin = 0;   %***
+                dc = num2cell(ones(size(dc)));
+            elseif any(any(cellfun(@abs,dc)<1e-14))   % transfer zero at s=0
+                wmin = 0;   %***
+                dc = num2cell(ones(size(dc)));
+            else
+                wmin=0; t = freqrespCell(varargin{i}, 10^wmin);
+                while cellfun(@(x,y) norm(x-y)/norm(y),t,dc) > 1e-2
+                    wmin=wmin-1; t = freqrespCell(varargin{i}, 1i*10^wmin);
+                end
+                while cellfun(@(x,y) norm(x-y)/norm(y),t,dc) < 1e-2
+                    wmin=wmin+1; t = freqrespCell(varargin{i}, 1i*10^wmin);
+                end
+                wmin=wmin-1;
+            end
+
+            %determine maximum frequency
+            wmax=0; t = freqrespCell(varargin{i}, 10^wmax);
+            while cellfun(@(x,y,z) norm(x-y)/norm(z),t,ft,dc) > 1e-6
+                wmax=wmax+1; t = freqrespCell(varargin{i}, 1i*10^wmax);
+            end
+            while cellfun(@(x,y,z) norm(x-y)/norm(z),t,ft,dc) < 1e-6
+                wmax=wmax-1; t = freqrespCell(varargin{i}, 1i*10^wmax);
+            end
+            wmax=wmax+1;
+
+            delta = (wmax-wmin)/19; % initial resolution (insert odd number only!)
+            omega = 10.^(wmin:delta:wmax);
+            m = freqrespCell(varargin{i}, 1i* omega);
+            
+            while(1)
+                % increase plot density until vertical change per step is below 1%
+                for k=1:length(omega)-1
+                    if cellfun(@(x) abs(abs(x(k)) - abs(x(k+1)))/(abs(x(k)) + abs(x(k+1))),m) > 0.01
+                        break
+                    end
+                end
+                if k==length(omega)-1
+                    break
+                end
+                % do not refine above 2000 points
+                if length(omega)>1000
+                    break
+                end
+                delta = delta/2;
+                omega = 10.^(wmin:delta:wmax);
+
+                % calculate new values of frequency response
+                temp=freqrespCell(varargin{i}, 1j*omega(2:2:length(omega)));
+                
+                % update array of results (insert new values)            
+                m=cellfun(@(x,y) [reshape([x(1:length(x)-1);y],1,2*length(x)-2),x(end)],m,temp,'UniformOutput',false);
+            end    
+        end
+        
+        % output
+        if nSys==1
+            % determine magnitude and phase from complex frequency response
+            mag = cellfun(@abs, m, 'UniformOutput',false);
+            phase = cellfun(@(x) unwrap(angle(x))*180/pi,m, 'UniformOutput',false);
+
+            % determine H_inf-norm (maximum magnitude)
+            [a,b]=cellfun(@max, mag);
+            [a,c]=max(a);
+            [H_inf_norm,d]=max(a);
+            H_inf_peakfreq=omega(b(c(d),d));
+            varargin{i}.H_inf_norm = max([varargin{i}.H_inf_norm, H_inf_norm]);
+            varargin{i}.H_inf_peakfreq = max([varargin{i}.H_inf_peakfreq, H_inf_peakfreq]);
+
+            mag=cell2mat(mag);
+            phase=cell2mat(phase);
+        end 
+        
+        if isempty(varargin{i}.Name)
+             varargin{i}.Name = inputname(i);
+        end
+        
+        % store system in caller workspace
+        if inputname(1)
+            assignin('caller', inputname(i), varargin{i});
+        end
+
+        % create frequency response data model
+        nInput=size(varargin{i}.B,2); %inputs
+        nOutput=size(varargin{i}.C,1); %outputs
+        
+        temp=zeros(nOutput, nInput,length(omega));
+        temp(:,:,:) = cell2mat(m);
+       
+        varargin{i} = frd(temp,omega,varargin{i}.Ts,...
+            'InputName',varargin{i}.InputName,'OutputName',varargin{i}.OutputName,...
+            'Name',varargin{i}.Name);  
+
+        % clear omega if input consists of more than one system without omega
+        if ~nnz(omegaIndex) && nSys>1
+           omega=[];
+        end
+    end
+end
+  
+% plot
+bode(varargin{:});
+end
+
+function m = freqrespCell(varargin)
+            m = freqresp(varargin{1},varargin{2});
+            warning('off', 'MATLAB:mat2cell:TrailingUnityVectorArgRemoved');
+            m = mat2cell(m,ones(size(m,1),1),ones(size(m,2),1),size(m,3));
+            m = cellfun(@(x) x(:,:),m, 'UniformOutput', false);
 end
