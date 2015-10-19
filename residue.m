80895a383389cc5007cee0d69d16fa1b0bd169ce
Jorge Luiz Moreira Silva <jorge.silva@tum.de>
2015-10-19 21:01:42 +0200

    residue.m -Commentaries that were not important to the understanding of the code were deleted. -The notation conj(B') was changed to B.' -The system.d is now updated after the function running. -One issue about this function was opened (please look at issues).

--- residue.m
+++ residue.m
@@ -12,7 +12,7 @@ function [r,p,d] = residue(sys)
 % For updates and further information please visit www.rt.mw.tum.de
 % ------------------------------------------------------------------
 % Authors:      Heiko Panzer (heiko@mytum.de), Sylvia Cremer
-% Last Change:  10 Nov 2011
+% Last Change:  19 Out 2015
 % ------------------------------------------------------------------
 
 % are poles and residues already available?
@@ -26,41 +26,42 @@ end
 
 tic
 %perform eigen-decomposition of system
-if sys.is_dae
-    [T,J] = eig(full(sys.A),full(sys.E));
-    if max(max(J))==inf
-    error('System contains algebraic states.')
+try
+    if sys.is_dae
+        
+        [T,J] = eig(full(sys.A),full(sys.E));
+        
+        if max(max(J))==inf
+            error('System contains algebraic states.')
+        end
+    else
+        [T,J] = eig(full(sys.A));
     end
-else
-    [T,J] = eig(full(sys.A));
+catch err
+    error('Computation of the eigenvalues and eigenvectors failed with message:%s',err.message);
 end
 
 % transform system to diagonal form
-p=conj(diag(J)');
+p=diag(J).';
 B=(sys.E*T)\sys.B;
 C=sys.C*T;
 d=sys.D;
 
 % calculate residues
 if sys.is_mimo
-    warning('Code edited 2013/10/16 HP');
     %mimo
-%     r = cell(sys.p, sys.m);
     r = cell(sys.n, 1);
     for i=1:sys.n
-%         for i=1:sys.p
-%             r{i,j} = C(i,:) .* conj(B(:,j)');
-%         end
         r{i} = C(:,i)*B(i,:);
     end
 else
     %siso
-    r = {C .* conj(B')};
+    r = {C .*B.'};
 end
 
 return
 
-% measure time required for diagonalization and computation of 
+% measure time required for diagonalization and computation of
 % 1000 values of the impulse response
 sys.SimulationTime=toc;
 tic
@@ -71,6 +72,7 @@ simtime=toc;
 sys.residues=r;
 sys.poles=p;
 sys.SimulationTime=sys.SimulationTime+1000*simtime;
+sys.d=d;
 if inputname(1)
     assignin('caller', inputname(1), sys);
 end
