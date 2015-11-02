5b8e41db26252ee35b0a0b6c1be33d1123400cc9
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-11-01 22:21:41 -0500

    fixing merge problems
    for some reason, the merge and restructuring has caused some changes to the sss functions to be lost along the way. I am trying to fix this manually, but possibly that are some other functions that were overseen

--- src/@sss/freqresp.m
+++ src/@sss/freqresp.m
@@ -1,11 +1,9 @@
-
-function G = freqresp(sys, s, varargin)
+function [G, omega, sys] = freqresp(varargin)
 % Evaluates complex transfer function of LTI systems
 % ------------------------------------------------------------------
 % G = freqresp(sys, s, varargin)
 % Inputs:       * sys: an sss-object containing the LTI system
 %               * s: vector of complex frequencies
-%    [optional] * number of input, number of output
 % Outputs:      * G: vector of complex frequency response values
 % ------------------------------------------------------------------
 % This file is part of the MORLAB_GUI, a Model Order Reduction and
@@ -13,90 +11,142 @@ function G = freqresp(sys, s, varargin)
 % Institute of Automatic Control, Technische Universitaet Muenchen
 % For updates and further information please visit www.rt.mw.tum.de
 % ------------------------------------------------------------------
-% Authors:      Heiko Panzer (heiko@mytum.de), Sylvia Cremer, Rudy Eid
-% Last Change:  02 Feb 2011
+% Authors:     Stefan Jaensch, Heiko Panzer (heiko@mytum.de), Sylvia Cremer, Rudy Eid
+%              Lisa Jeschek
 % ------------------------------------------------------------------
 
-[A,B,C,D,E] = ABCDE(sys);
-m=sys.m; p=sys.p;
-if nargin >= 4
-    % I/O-pair selected
-    if ~isempty(varargin{1}) && ~isnan(varargin{1})
-        B=B(:,varargin{1});
-        D=D(:,varargin{1});
-        m=size(B,2);
-    end
-    if ~isempty(varargin{2}) && ~isnan(varargin{2})
-        C=C(varargin{2},:);
-        D=D(varargin{2},:);
-        p=size(C,1);
-    end
+sys= varargin{1};
+
+omegaIndex = cellfun(@isfloat,varargin);
+if ~isempty(omegaIndex) && nnz(omegaIndex)
+    omega = varargin{omegaIndex};
+    varargin(omegaIndex)=[];
+else
+    [~,omega,sys] = getFreqRange(sys);
 end
 
+if (sys.Ts==0) % Convert frequency to either laplace or z variable
+    s = 1i* omega;
+else
+    s = exp(1i* omega*sys.Ts);
+end
+
+for ii=1:length(varargin)
+    sys= varargin{ii};
+    m=sys.m; p=sys.p; n=sys.n;
+    [A,B,C,D,E] = dssdata(sys);
+    G=zeros(p,m,length(s));
+    if not(sys.isBig)
+        for i=1:length(s)
+            G(:,:,i) = freqresp_local(A,B,C,D,E, s(i),n);
+        end
+    else
+        parfor i=1:length(s)
+            G(:,:,i) = freqresp_local(A,B,C,D,E, s(i),n);
+        end
+    end
+end
+end
+
+function G = freqresp_local(A,B,C,D,E, s,n)
 % calculate value of transfer function for given vector of frequencies
-G=zeros(p,m,length(s));
-for i=1:length(s)
-    if isinf(s(i)) || sys.n==0
-        G(:,:,i) = D;
-        continue
-    end
-    %   [L,U]=lu(A - s(i)*E, 'vector');
-    [L,U,k,l,S]=lu(A - s(i)*E, 'vector');
-    warning('OFF', 'MATLAB:nearlySingularMatrix')
-    b=S\B; b=b(k,:);
-    x=L\b;
-    x(l,:)=U\x;
-    warning('ON', 'MATLAB:nearlySingularMatrix')
-    G(:,:,i) = -C*x + D;
+
+if isinf(s) || n==0
+    G = D;
+    return
 end
 
-warning('off', 'MATLAB:mat2cell:TrailingUnityVectorArgRemoved');
-G = mat2cell(G,ones(p,1),ones(m,1),length(s));
-G = cellfun(@(x) x(:,:),G, 'UniformOutput', false);
+[L,U,k,l,S]=lu(A - s*E, 'vector');
+warning('OFF', 'MATLAB:nearlySingularMatrix')
+b=S\B; b=b(k,:);
+x=L\b;
+x(l,:)=U\x;
+warning('ON', 'MATLAB:nearlySingularMatrix')
+G = -C*x + D;
+end
+
+
+function [m,omega,sys] = getFreqRange(sys) %TODO: fix for Ts~=0
+
+% --------- frequency range needs to be chosen ---------
+dc = freqrespCell(sys,0);    % G(0)=DCgain
+ft = freqrespCell(sys,inf);  % G(inf)=feedthrough
+
+%determine minimum frequency
+if any(any(cellfun(@isinf,dc))) || any(any(cellfun(@isnan,dc))) % pole at s=0
+    wmin = 0;   %***
+    dc = num2cell(ones(size(dc)));
+elseif any(any(cellfun(@abs,dc)<1e-14))   % transfer zero at s=0
+    wmin = 0;   %***
+    dc = num2cell(ones(size(dc)));
+else
+    wmin=0; t = freqrespCell(sys, -1i*10^wmin);
+    while cellfun(@(x,y) norm(x-y)/norm(y),t,dc) > 1e-2
+        wmin=wmin-1; t = freqrespCell(sys, 10^wmin);
+    end
+    while cellfun(@(x,y) norm(x-y)/norm(y),t,dc) < 1e-2
+        wmin=wmin+1; t = freqrespCell(sys, 10^wmin);
+    end
+    wmin=wmin-1;
+end
+
+%determine maximum frequency
+wmax=0; t = freqrespCell(sys, -1i*10^wmax);
+while cellfun(@(x,y,z) norm(x-y)/norm(z),t,ft,dc) > 1e-6
+    wmax=wmax+1; t = freqrespCell(sys, 10^wmax);
+end
+while cellfun(@(x,y,z) norm(x-y)/norm(z),t,ft,dc) < 1e-6
+    wmax=wmax-1; t = freqrespCell(sys, 10^wmax);
+end
+wmax=wmax+1;
+
+delta = (wmax-wmin)/19; % initial resolution (insert odd number only!)
+omega = 10.^(wmin:delta:wmax);
+m = freqrespCell(sys, omega);
+
+while(1)
+    % increase plot density until vertical change per step is below 1%
+    for k=1:length(omega)-1
+        if cellfun(@(x) abs(abs(x(k)) - abs(x(k+1)))/(abs(x(k)) + abs(x(k+1))),m) > 0.01
+            break
+        end
+    end
+    if k==length(omega)-1
+        break
+    end
+    % do not refine above 2000 points
+    if length(omega)>1000
+        break
+    end
+    delta = delta/2;
+    omega = 10.^(wmin:delta:wmax);
     
-%     else
-% %         if find(E-eye(size(A)))
-% %             % [AA,BB,Q,Z] = HESS(A,B) => Q*A*Z = AA,  Q*B*Z = BB.
-% %             [A, E, Q, Z] = hess(A, E);
-% %             C = C*Q;
-% %             B = Z*B;
-% %             for i=1:length(s)
-% %                 [L,U] = lu(full(A - s(i)*E));
-% %                 G(i) = -C/U*(L\B) + D;
-% %             end
-% %         else
-% %             [T, A] = balance(A);
-% %             B = T \ B;
-% %             C = C * T;
-% %             [P, A] = hess(A);
-% %             C = C*P;
-% %             B = P'*B;
-% %             sys = struct('A', A, 'B', B, 'C', C, 'D', D, 'E', E);
-% %             g = ssfresp(A,B,[],[],[],s);
-% %             g = reshape(g,[length(A) numel(s)]);
-% %             z = C*g  + D;
-%             if ~all(isfield(sys,{'residuen','poles'}))
-%                 [temp1, temp2,temp3,sys] = residuen(sys);
-%                 clear('temp1','temp2','temp3')
-%             end
-%             res=sys.residuen;
-%             p=sys.poles;
-%             if ~isempty(in)
-%                 res=transpose({res{:,in}});
-%             end
-%             if ~isempty(out)
-%                 res={res{out,:}};
-%             end
-%             G=cellfun(@(x) transpose(sum(transpose(ones(size(s)))*x./(transpose(s)*ones(size(p))-ones(size(s))'*p),2)+D),res,'UniformOutput',false);
-% 
-% %            res=cell2mat(res);
-% %            for i=1:length(s)
-% %                 G(i) = sum( res./(s(i)-p));
-% % %                 if isreal(s(i)) % remove numerical noise in imaginary part
-% % %                     G(i) = real(G(i)) + D;
-% % %                 end
-% %             end
-% %         end
-% 
-%     end
+    % calculate new values of frequency response
+    temp=freqrespCell(sys, omega(2:2:length(omega)));
+    
+    % update array of results (insert new values)
+    m=cellfun(@(x,y) [reshape([x(1:length(x)-1);y],1,2*length(x)-2),x(end)],m,temp,'UniformOutput',false);
+end
+
+% determine magnitude and phase from complex frequency response
+mag = cellfun(@abs, m, 'UniformOutput',false);
+
+% determine H_inf-norm (maximum magnitude)
+[a,b]=cellfun(@max, mag);
+[a,c]=max(a);
+[H_inf_norm,d]=max(a);
+H_inf_peakfreq=omega(b(c(d),d));
+% Issue: the values get assigned but this does not affect the original
+% sss object as it is called by value not by reference as @sss is not a
+% handle class.
+sys.H_inf_norm = max([sys.H_inf_norm, H_inf_norm]);
+sys.H_inf_peakfreq = max([sys.H_inf_peakfreq, H_inf_peakfreq]);
+
+end
+
+function [m, omega] = freqrespCell(varargin)
+[m, omega] = freqresp(varargin{:});
+warning('off', 'MATLAB:mat2cell:TrailingUnityVectorArgRemoved');
+m = mat2cell(m,ones(size(m,1),1),ones(size(m,2),1),size(m,3));
+m = cellfun(@(x) x(:,:),m, 'UniformOutput', false);
 end
--- src/@sss/residue.m
+++ src/@sss/residue.m
@@ -1,76 +1,142 @@
-function [r,p,d] = residue(sys)
-% Computes residues, poles and feedthrough of an LTI system
-% ------------------------------------------------------------------
-% [r,p,d] = residue(sys)
-% Inputs:       * sys: an sss-object containing the LTI system
-% Outputs:      * residues r, poles p and feedthrough d, such that
-%                   G(s) = r_i/(p_i+s) + d
-% ------------------------------------------------------------------
-% This file is part of the MORLAB_GUI, a Model Order Reduction and
-% System Analysis Toolbox developed at the
-% Institute of Automatic Control, Technische Universitaet Muenchen
-% For updates and further information please visit www.rt.mw.tum.de
-% ------------------------------------------------------------------
-% Authors:      Heiko Panzer (heiko@mytum.de), Sylvia Cremer
-% Last Change:  10 Nov 2011
-% ------------------------------------------------------------------
+function [r,p,d] = residue(sys, Opts)
+% RESIDUE - Computes residues, poles and feedthrough of an LTI system
+% 
+% Syntax:
+%       RESIDUE(sys)
+%       [r] = RESIDUE(sys)
+%       [r,p] = RESIDUE (sys)
+%       [r,p,d] = RESIDUE(sys,Opts)
+%
+% Description:
+%       This function computes the pole/residual representation of a
+%       rational transfer function (SISO or MIMO). 
+%
+%       It can either return the residuals themselves, or the residual 
+%       directions (low rank factors), useful especially in the MIMO 
+%       setting.
+%
+%       The computation requires the complete eigendecomposition of the
+%       system, so for large scale problems it might take a while.
+%
+%       The output r is a cell array of residuals or residual directions
+%       depending on the option rType = {'res' (def), 'dir'}
+%       For rType = 'res' r is a cell array of dimension nx1 with the
+%       residual r{k} for each pole p(k)
+%       For rType = 'dir' r is a cell array of dimension 1x2 with the
+%       output residual matrix Chat = r{1} and the input residual matrix 
+%       Bhat = r{2}. The residues can be computed using 
+%       r{k} = Chat(:,k)*Bhat(k,:).
+%
+% Input Arguments:
+%		-sys: sss-object of the LTI system
+%       -[optional] Opts: Options
+%           -rType: define the output format of the residue
+%               -rType='res' (standard): r is a cell array of dimension nx1 with the
+%               residual r{k} for each pole p(k)
+%               -rType='dir': r is a cell array of dimension 1x2 with the
+%               output residual matrix Chat = r{1} and the input residual matrix 
+%               Bhat = r{2}
+%
+% Output Arguments:
+%       -r: cell of residuals (format depends on Opts.rType)
+%       -p: eigenvalues
+%       -d: feedthrough
+%
+% Examples:
+%		To compute the residuals of a SISO or MIMO system such that 
+%//       G(s) = sum_i(r_i/(p_i+s) + d) 
+%       use
+%
+%>		load build; 
+%>		sys = sss(A,B,C);
+%>		[r,p,d] = residue(sys);
+%
+%       To get the residue directions instead of the residues, use the
+%       option Opts.rType = 'dir'
+%
+%>		load build; 
+%>		sys = sss(A,B,C);
+%>      Opts.rType = 'dir';
+%>		[r,p,d] = residue(sys,Opts);
+%
+% See also: 
+%		sss, eig
+%
+% References:
+%		* *[1] Bryson (1994)*, Control of Spacecraft and Aircraft
+%
+%------------------------------------------------------------------
+%   This file is part of <a href="matlab:docsearch sssMOR">sssMOR</a>, a Sparse State Space, Model Order 
+%   Reduction and System Analysis Toolbox developed at the Chair of 
+%   Automatic Control, Technische Universitaet Muenchen. For updates 
+%   and further information please visit <a href="https://www.rt.mw.tum.de/">www.rt.mw.tum.de</a>
+%   For any suggestions, submission and/or bug reports, mail us at
+%                     -> <a href="mailto:sssMOR@rt.mw.tum.de">sssMOR@rt.mw.tum.de</a> <-
+%
+%   More Toolbox Info by searching <a href="matlab:docsearch sssMOR">sssMOR</a> in the Matlab Documentation
+%
+%------------------------------------------------------------------
+% Authors:      Heiko Panzer, Sylvia Cremer, Alessandro Castagnotto
+% Email:        <a href="mailto:sssMOR@rt.mw.tum.de">sssMOR@rt.mw.tum.de</a>
+% Website:      <a href="https://www.rt.mw.tum.de/">www.rt.mw.tum.de</a>
+% Work Adress:  Technische Universitaet Muenchen
+% Last Change:  28 Oct 2015
+% Copyright (c) 2015 Chair of Automatic Control, TU Muenchen
+%------------------------------------------------------------------
+
+Def.rType = 'res';
+
+if ~exist('Opts','var') || isempty(Opts)
+    Opts = Def;
+else
+    Opts = parseOpts(Opts,Def);
+end   
 
 % are poles and residues already available?
 if ~isempty(sys.poles) && ~isempty(sys.residues)
     p=sys.poles;
     r=sys.residues;
-    d=sys.d;
+    d=sys.D;
     return
 end
 
-
-tic
 %perform eigen-decomposition of system
-if sys.isdescriptor
-    [T,J] = eig(full(sys.A),full(sys.E));
-    if max(max(J))==inf
-    error('System contains algebraic states.')
-    end
-else
-    [T,J] = eig(full(sys.A));
+try
+    [T,J] = eig(sys);
+catch err
+    error('Computation of the eigenvalues and eigenvectors failed with message:%s',err.message);
 end
 
 % transform system to diagonal form
-p=conj(diag(J)');
+p=diag(J).';
+if issparse(T)
+    rcondNumber = 1/condest(T);
+else
+    rcondNumber=rcond(T);
+end
+if rcondNumber<eps
+    warning('Matrix of eigenvectors is close to singular or badly scaled. Results may be inaccurate. RCOND =',num2str(rcondNumber));
+end
 B=(sys.E*T)\sys.B;
 C=sys.C*T;
 d=sys.D;
 
 % calculate residues
-if sys.is_mimo
-    warning('Code edited 2013/10/16 HP');
-    %mimo
-%     r = cell(sys.p, sys.m);
-    r = cell(sys.n, 1);
-    for i=1:sys.n
-%         for i=1:sys.p
-%             r{i,j} = C(i,:) .* conj(B(:,j)');
-%         end
-        r{i} = C(:,i)*B(i,:);
-    end
-else
-    %siso
-    r = {C .* conj(B')};
+r = cell(1,sys.n);
+for i=1:sys.n
+    r{i} = C(:,i)*B(i,:);
 end
-
-return
-
-% measure time required for diagonalization and computation of 
-% 1000 values of the impulse response
-sys.SimulationTime=toc;
-tic
-cellfun(@(x) sum((diag(x)*conj(exp(3'*p))'),1),r,'UniformOutput',false);
-simtime=toc;
 
 % store results to caller workspace
 sys.residues=r;
 sys.poles=p;
-sys.SimulationTime=sys.SimulationTime+1000*simtime;
 if inputname(1)
     assignin('caller', inputname(1), sys);
 end
+
+% return the residual directions instead of the residuals
+if strcmp(Opts.rType,'dir')
+    clear r  
+    r = {C, B};  
+end
+return
