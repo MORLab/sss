7c1c258fcc6a256e2a4b1a62dc22fbd66f06dfc8
Thomas Emmert <emmert@tfd.mw.tum.de>
2015-11-03 11:56:59 +0100

    -moved disp function into separate file -cosmetics -Made discrete vs. continuous time similar to ss

--- src/@sss/sss.m
+++ src/@sss/sss.m
@@ -379,53 +379,6 @@ classdef sss
                 error('Input label vector too long.')
             end
         end
-        
-        %% Display functions
-        function str = dispMorInfo(sys)
-            if isempty(sys.morInfo)
-                str=[];
-            else
-                str = ['Created by MOR on ' datestr(sys.morInfo.time) '.' char(10) ...
-                    'Reduction Method: ' sys.morInfo.method  '.' char(10) ...
-                    'Original system: ' sys.morInfo.orgsys '.'];
-            end
-        end
-        
-        function varargout = disp(sys)
-            mc = metaclass(sys);
-            str = [];
-            if ~isempty(mc.Name) && ~isempty(sys.Name)
-                str = [mc.Name ' Model ' sys.Name];
-            end
-            if sys.isDae
-                str = [str '(DAE)'];
-            elseif sys.isDescriptor
-                str = [str '(DSS)'];
-            else
-                str = [str '(SSS)'];
-            end
-            if sys.isSiso
-                str = [str '(SISO)'];
-            elseif sys.isSimo
-                str = [str '(SIMO)'];
-            elseif sys.isMiso
-                str = [str '(MISO)'];
-            elseif sys.isMimo
-                str = [str '(MIMO)'];
-            end
-            str = [str  char(10), num2str(sys.n) ' states, ' num2str(sys.m) ...
-                ' inputs, ' num2str(sys.p) ' outputs'];
-            str = [str  char(10) 'sampling time: ' num2str(sys.Ts)];
-            if ~isempty(sys.morInfo)
-                str = [str char(10) sys.dispMorInfo];
-            end
-            if nargout>0
-                varargout = {str};
-            else
-                str = strrep(str, char(10), [char(10) '  ']);
-                disp(['  ' str char(10)]);
-            end
-        end
     end
     
     methods(Access = private)
