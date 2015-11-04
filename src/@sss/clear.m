178064c382b6047c66cc5653273427030aa4c5dd
Maria Cruz Varona <maria.cruz@tum.de>
2015-11-04 11:21:44 +0100

    Update header of "clear".
    Change and adapt it, if desired

--- src/@sss/clear.m
+++ src/@sss/clear.m
@@ -1,19 +1,37 @@
 function sys = clear(sys)
-%clear deletes all state space dimension related properties
-% ------------------------------------------------------------------
-% [sys] = clear(sys)
-% Input:        * sys: an sss-object containing the LTI system
-% Output:       * sys: the same system with cleared system matrices and
-%                      labels
-% ------------------------------------------------------------------
-% This file is part of the MORLAB_GUI, a Model Order Reduction and
-% System Analysis Toolbox developed at the
-% Institute of Automatic Control, Technische Universitaet Muenchen
-% For updates and further information please visit www.rt.mw.tum.de
-% ------------------------------------------------------------------
-% Authors:      Thomas Emmert (emmert@tfd.tum.de)
-% Last Change:  25 Mar 2015
-% ------------------------------------------------------------------
+% CLEAR - Deletes all state-space dimension related properties
+%
+% Syntax:
+%       sys = CLEAR(sys)
+%
+% Description:
+%       sys = clear(sys) deletes the system matrices and lables of the
+%       sparse state-space (sss)-object sys.
+%
+% Input Arguments:
+%       -sys: an sss-object containing the LTI system
+%
+% Output Arguments:
+%       -sys: the same system with cleared system matrices and labels
+%
+%------------------------------------------------------------------
+% This file is part of <a href="matlab:docsearch sss">sss</a>, a Sparse State-Space and System Analysis 
+% Toolbox developed at the Chair of Automatic Control in collaboration
+% with the Chair of Thermofluid Dynamics, Technische Universitaet Muenchen. 
+% For updates and further information please visit <a href="https://www.rt.mw.tum.de/">www.rt.mw.tum.de</a>
+% For any suggestions, submission and/or bug reports, mail us at
+%                   -> <a href="mailto:sssMOR@rt.mw.tum.de">sssMOR@rt.mw.tum.de</a> <-
+%
+% More Toolbox Info by searching <a href="matlab:docsearch sssMOR">sssMOR</a> in the Matlab Documentation
+%
+%------------------------------------------------------------------
+% Authors:      Thomas Emmert (emmert@tfd.mw.tum.de)
+% Email:        <a href="mailto:sssMOR@rt.mw.tum.de">sssMOR@rt.mw.tum.de</a>
+% Website:      <a href="https://www.rt.mw.tum.de/">www.rt.mw.tum.de</a>
+% Work Adress:  Technische Universitaet Muenchen
+% Last Change:  04 Nov 2015
+% Copyright (c) 2015 Chair of Automatic Control, TU Muenchen
+%------------------------------------------------------------------
 
 sys.A=[];sys.B=[];sys.C=[];sys.D=[];sys.E=[];
 sys.InputName=[];
