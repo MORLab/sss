28a4689e0e52d4c23db77bf3cc974cfab9febfb1
Thomas Emmert <emmert@tfd.mw.tum.de>
2015-11-06 17:50:08 +0100

    - Added option to multiply with static gain - consider io names - retain type of sss opject and all properties.

--- src/@sss/mtimes.m
+++ src/@sss/mtimes.m
@@ -1,4 +1,4 @@
-function prod = mtimes(sys1, sys2)
+function sys1 = mtimes(sys1, sys2)
 % MTIMES - mtimes computes the product of two LTI systems: u-->sys2-->sys1-->y
 %
 % Syntax:
@@ -39,16 +39,46 @@ function prod = mtimes(sys1, sys2)
 % Copyright (c) 2015 Chair of Automatic Control, TU Muenchen
 %------------------------------------------------------------------
 
-if (sys1.n==0 && isempty(sys1.D)) || (sys2.n==0 && isempty(sys2.D))
-    prod = sss([], [], []);
-    return
+
+%% Convert numeric gain to sss
+if isnumeric(sys1)
+    D = sparse(sys1);
+    if isvector(D)
+        % Apply static gain to all channels
+        D = speye(sys2.p)*diag(D);
+    end
+    % Give all properties to the returned model
+    sys1 = sys2.clear;
+    sys1.D = D;
+    sys1.y = sys2.y;
 end
-if sys2.p ~= sys1.m
-    error('Number of outputs of sys1 does not match number of inputs of sys2.')
+if isnumeric(sys2)
+    D = sparse(sys2);
+    if isvector(D)
+        % Apply static gain to all channels
+        D = speye(sys1.m)*diag(D);
+    end
+    sys2 = sss(D);
+    sys2.u = sys1.u;
 end
 
-prod = sss([sys1.A sys1.B*sys2.C; sparse(sys2.n,sys1.n) sys2.A], ...
-          [sys1.B*sys2.D; sys2.B], ...
-          [sys1.C, sys1.D*sys2.C], ...
-          sys1.D*sys2.D, ...
-          [sys1.E sparse(sys1.n,sys2.n); sparse(sys2.n,sys1.n) sys2.E]);
+if sys1.m ~= sys2.p
+    error('Number of inputs of sys1 does not match number of outputs of sys2.')
+end
+
+% Store input and output names of the resulting system
+u = sys2.u;
+y = sys1.y;
+
+A = [sys1.A sys1.B*sys2.C; sparse(sys2.n,sys1.n) sys2.A];
+B = [sys1.B*sys2.D; sys2.B];
+C = [sys1.C, sys1.D*sys2.C];
+D = sys1.D*sys2.D;
+E = [sys1.E sparse(sys1.n,sys2.n); sparse(sys2.n,sys1.n) sys2.E];
+
+sys1.A=A; sys1.B=B; sys1.C=C; sys1.D=D; sys1.E=E;
+
+sys1.u = u;
+sys1.y = y;
+
+end
