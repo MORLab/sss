5a6900de53a8df12606ee5a3ca03dcb01d87bf91
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-11-07 01:52:33 -0500

    freqresp - 3D arrays instead of cellfun

--- src/@sss/freqresp.m
+++ src/@sss/freqresp.m
@@ -51,7 +51,8 @@ if ~isempty(omegaIndex) && nnz(omegaIndex)
     omega = varargin{omegaIndex};
     varargin(omegaIndex)=[];
 else
-    [~,omega,sys] = getFreqRange(sys);
+    [G,omega,sys] = getFreqRange(sys);
+    return
 end
 
 if (sys.Ts==0) % Convert frequency to either laplace or z variable
@@ -88,88 +89,62 @@ warning('ON', 'MATLAB:nearlySingularMatrix')
 G = -C*x + D;
 end
 
-
 function [m,omega,sys] = getFreqRange(sys)
 
 % --------- frequency range needs to be chosen ---------
-dc = freqrespCell(sys,0);    % G(0)=DCgain
-ft = freqrespCell(sys,inf);  % G(inf)=feedthrough
+dc = freqresp(sys,0);    % G(0)=DCgain
+ft = freqresp(sys,inf);  % G(inf)=feedthrough
 
 %determine minimum frequency
-if any(any(cellfun(@isinf,dc))) || any(any(cellfun(@isnan,dc))) % pole at s=0
+if any(any(isinf(dc))) || any(any(isnan(dc))) % pole at s=0
     wmin = 0;   %***
-    dc = num2cell(ones(size(dc)));
-elseif any(any(cellfun(@abs,dc)<1e-14))   % transfer zero at s=0
+    dc = ones(size(dc));
+elseif any(any(abs(dc<1e-14)))   % transfer zero at s=0
     wmin = 0;   %***
-    dc = num2cell(ones(size(dc)));
+    dc = ones(size(dc));
 else
-    wmin=0; t = freqrespCell(sys, -1i*10^wmin);
+    wmin=0; t = freqresp(sys, -1i*10^wmin);
     while cellfun(@(x,y) norm(x-y)/norm(y),t,dc) > 1e-2
-        wmin=wmin-1; t = freqrespCell(sys, 10^wmin);
+        wmin=wmin-1; t = freqresp(sys, 10^wmin);
     end
     while cellfun(@(x,y) norm(x-y)/norm(y),t,dc) < 1e-2
-        wmin=wmin+1; t = freqrespCell(sys, 10^wmin);
+        wmin=wmin+1; t = freqresp(sys, 10^wmin);
     end
     wmin=wmin-1;
 end
 
 %determine maximum frequency
-wmax=0; t = freqrespCell(sys, -1i*10^wmax);
-while cellfun(@(x,y,z) norm(x-y)/norm(z),t,ft,dc) > 1e-6
-    wmax=wmax+1; t = freqrespCell(sys, 10^wmax);
+wmax=0; t = freqresp(sys, -1i*10^wmax);
+while norm(t-ft)/norm(dc) > 1e-6
+    wmax=wmax+1; t = freqresp(sys, 10^wmax);
 end
-while cellfun(@(x,y,z) norm(x-y)/norm(z),t,ft,dc) < 1e-6
-    wmax=wmax-1; t = freqrespCell(sys, 10^wmax);
+while norm(t-ft)/norm(dc) < 1e-6
+    wmax=wmax-1; t = freqresp(sys, 10^wmax);
 end
 wmax=wmax+1;
 
 delta = (wmax-wmin)/19; % initial resolution (insert odd number only!)
 omega = 10.^(wmin:delta:wmax);
-m = freqrespCell(sys, omega);
+m = freqresp(sys, omega);
 
 while(1)
     % increase plot density until vertical change per step is below 1%
-    for k=1:length(omega)-1
-        if cellfun(@(x) abs(abs(x(k)) - abs(x(k+1)))/(abs(x(k)) + abs(x(k+1))),m) > 0.01
-            break
-        end
-    end
-    if k==length(omega)-1
+    if ~any(any(abs(diff(m,1,3))./abs(m(:,:,1:end-1)) > 0.01))
+        % relative accuracy achieved
         break
     end
-    % do not refine above 1000 points
-    if length(omega)>1000
+    if length(omega)>1000 % do not refine above 1000 points
         break
     end
     delta = delta/2;
     omega = 10.^(wmin:delta:wmax);
     
     % calculate new values of frequency response
-    temp=freqrespCell(sys, omega(2:2:length(omega)));
+    temp=freqresp(sys, omega(2:2:length(omega)));
     
     % update array of results (insert new values)
-    m=cellfun(@(x,y) [reshape([x(1:length(x)-1);y],1,2*length(x)-2),x(end)],m,temp,'UniformOutput',false);
+    mOld = m; m = zeros(size(m,1),size(m,2),length(omega));
+    m(:,:,2:2:length(omega)) = temp; m(:,:,1:2:end) = mOld;
 end
 
-% determine magnitude and phase from complex frequency response
-mag = cellfun(@abs, m, 'UniformOutput',false);
-
-% determine H_inf-norm (maximum magnitude)
-[a,b]=cellfun(@max, mag);
-[a,c]=max(a);
-[H_inf_norm,d]=max(a);
-H_inf_peakfreq=omega(b(c(d),d));
-% Issue: the values get assigned but this does not affect the original
-% sss object as it is called by value not by reference as @sss is not a
-% handle class.
-sys.H_inf_norm = max([sys.H_inf_norm, H_inf_norm]);
-sys.H_inf_peakfreq = max([sys.H_inf_peakfreq, H_inf_peakfreq]);
-
-end
-
-function [m, omega] = freqrespCell(varargin)
-[m, omega] = freqresp(varargin{:});
-warning('off', 'MATLAB:mat2cell:TrailingUnityVectorArgRemoved');
-m = mat2cell(m,ones(size(m,1),1),ones(size(m,2),1),size(m,3));
-m = cellfun(@(x) x(:,:),m, 'UniformOutput', false);
 end
