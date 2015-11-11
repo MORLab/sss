fc53819fcabfd631a0c3fb8c43959e4a10b9a14f
Jorge Luiz Moreira Silva <jorge.silva@tum.de>
2015-11-11 02:58:57 +0100

    impulse.m and step.m updated: -Now their outputs are the same as in the built-in matlab functions. -They accept that the system has ONE zero. -They call built-in matlab function if the system is not diagonalizable.

--- src/@sss/impulse.m
+++ src/@sss/impulse.m
@@ -1,4 +1,4 @@
-function  [varargout] = impulse(sys, varargin)
+function  [temp,t] = impulse(sys, varargin)
 % IMPULSE - Computes and/or plots the impulse response of a sparse LTI system
 %
 % Syntax:
@@ -19,13 +19,12 @@ function  [varargout] = impulse(sys, varargin)
 %       -h, t: vectors containing impulse response and time vector
 %
 % Examples:
-%       TODO
+%       load build.mat
+%       sys=sss(A,B,C);
+%       impulse(sys);
 %
 % See Also:
-%       TODO
-%
-% References:
-%       TODO
+%       sss/residue, sss/step
 %
 %------------------------------------------------------------------
 % This file is part of <a href="matlab:docsearch sss">sss</a>, a Sparse State-Space and System Analysis
@@ -57,52 +56,49 @@ else
         assignin('caller', inputname(1), sys);
     end
 end
+builtinMATLAB=0;
+if condest(sys.E)>1/(100*eps) %Verify if E is singular
+    warning('Matrix E is close to singular or badly scaled: running the MATLAB built-in function step');
+    builtinMATLAB=1;
+end
+if not(builtinMATLAB)
+    for i=1:numel(p) %Verify if the poles are repeated
+        if p(i)~=0
+            numberOfRepeatedPoles=sum(abs((p-p(i))/p(i))<100*eps);
+        else
+            numberOfRepeatedPoles=sum(abs(p-p(i))<100*eps);
+        end
+        if numberOfRepeatedPoles>1
+            builtinMATLAB=1;
+            warning('System is not diagnoalizable because of repetition of eigenvalues: running the MATLAB built-in function step');
+            break;
+        end
+    end
+end
 
 options=varargin;
-if nargin>1 && strcmp(class(options{1}), 'double')
+if nargin>1 && isa(options{1},'double')
     t=varargin{1};
     options(1)=[];
 end
 
-% is time vector given?
-if exist('t', 'var') && ~isempty(t)
-    % Change the format of res to work with the following code
-    resOld = res; clear res; res = cell(sys.p,sys.m);
-    for iOut = 1:sys.p
-        for jIn = 1:sys.m
-            res{iOut,jIn} = [];
-            for kPole = 1:length(p)
-                res{iOut,jIn} = [res{iOut,jIn}, resOld{kPole}(iOut,jIn)];
-            end
+
+if builtinMATLAB
+    if exist('t','var')
+        if nargout>0
+            [temp,t]=impulse(ss(sys),t);
+        else
+            impulse(ss(sys),t);
         end
-    end
-    
-    if size(t,1)>1 && size(t,2)==1
-        t=t';
-    elseif size(t,1)>1 && size(t,2)>1
-        error('t must be a vector.');
-    end
-    
-    % calculate impulse response
-    h=cellfun(@(x) sum((diag(x)*conj(exp(t'*p))'),1),res,'UniformOutput',false);
-    h=cellfun(@real,h,'UniformOutput',false);
-    
-else
-    % no, retrieve time values automatically
-    
-    % is decayTime already available?
-    if ~isempty(sys.decayTime)
-        tmax = sys.decayTime;
     else
-        tmax = decayTime(sys);
-        % store system to caller workspace
-        if inputname(1)
-            assignin('caller', inputname(1), sys);
+        if nargout>0
+            [temp,t]=impulse(ss(sys),t);
+        else
+            impulse(ss(sys),t);
         end
     end
-    delta = tmax/999;
-    t = 0:delta:tmax;
-    
+        
+else
     % Change the format of res to work with the following code
     %   original: res = {res1, res2,... resN} where resK = res(p(k)) is the
     %           (possibly matrix-valued) residual to p(k)
@@ -118,27 +114,59 @@ else
         end
     end
     
-    % calculate impulse response
-    %     cellfun(@(x) sum((diag(x)*conj(exp(t'*p))'),1),res,'UniformOutput',false);
-    h=cellfun(@(x)   sum(diag(x)*transpose(exp(t'*p)),1),res,'UniformOutput',false);
-    h=cellfun(@real,h,'UniformOutput',false);
-    
-    % increase resolution as long as rel. step size is too large
-    ex=1;
-    while 1
-        refine=0;
-        for iOut=1:sys.p
-            for jIn=1:sys.m
-                m=h{iOut,jIn};
-                for k=2:length(m)-1
-                    if abs(abs(m(k)) - abs(m(k+1)))/(abs(m(k)) + abs(m(k+1))) > 0.5
-                        delta=delta/2;
-                        t=0:delta:tmax;
-                        t_temp=t(2:2:end);
-                        temp=cellfun(@(x) sum((diag(x)*conj(exp(t_temp'*p))'),1),res,'UniformOutput',false);
-                        temp=cellfun(@real,temp,'UniformOutput',false);
-                        h=cellfun(@(x,y) [reshape([x(1:length(x)-1); y],1,2*length(x)-2),x(end)],h,temp,'UniformOutput',false);
-                        refine=1;
+    % is time vector given?
+    if exist('t', 'var') && ~isempty(t)
+        % Change the format of res to work with the following code
+        
+        if size(t,1)>1 && size(t,2)==1
+            t=t';
+        elseif size(t,1)>1 && size(t,2)>1
+            error('t must be a vector.');
+        end
+        
+        % calculate impulse response
+        h=cellfun(@(x) sum((diag(x)*conj(exp(t'*p))'),1),res,'UniformOutput',false);
+        h=cellfun(@real,h,'UniformOutput',false);
+        
+    else
+        % no, retrieve time values automatically
+        
+        % is decayTime already available?
+        if ~isempty(sys.decayTime)
+            tmax = sys.decayTime;
+        else
+            tmax = decayTime(sys);
+            % store system to caller workspace
+            if inputname(1)
+                assignin('caller', inputname(1), sys);
+            end
+        end
+        delta = tmax/999;
+        t = 0:delta:tmax;
+        
+        h=cellfun(@(x)   sum(diag(x)*transpose(exp(t'*p)),1),res,'UniformOutput',false);
+        h=cellfun(@real,h,'UniformOutput',false);
+        
+        % increase resolution as long as rel. step size is too large
+        ex=1;
+        while 1
+            refine=0;
+            for iOut=1:sys.p
+                for jIn=1:sys.m
+                    m=h{iOut,jIn};
+                    for k=2:length(m)-1
+                        if abs(abs(m(k)) - abs(m(k+1)))/(abs(m(k)) + abs(m(k+1))) > 0.5
+                            delta=delta/2;
+                            t=0:delta:tmax;
+                            t_temp=t(2:2:end);
+                            temp=cellfun(@(x) sum((diag(x)*conj(exp(t_temp'*p))'),1),res,'UniformOutput',false);
+                            temp=cellfun(@real,temp,'UniformOutput',false);
+                            h=cellfun(@(x,y) [reshape([x(1:length(x)-1); y],1,2*length(x)-2),x(end)],h,temp,'UniformOutput',false);
+                            refine=1;
+                            break
+                        end
+                    end
+                    if refine
                         break
                     end
                 end
@@ -146,70 +174,69 @@ else
                     break
                 end
             end
-            if refine
+            if ~refine
+                break
+            end
+            ex=ex+1;
+            if ex==5
                 break
             end
         end
-        if ~refine
-            break
-        end
-        ex=ex+1;
-        if ex==5
-            break
-        end
     end
-end
-
-if nargout>0
-    temp=zeros(length(t),sys.p,sys.m);
-    for i=1:sys.p
-        for j=1:sys.m
-            temp(:,i,j)=h{i,1}';
+    
+    if nargout>0
+        temp=zeros(length(t),sys.p,sys.m);
+        for i=1:sys.p
+            for j=1:sys.m
+                temp(:,i,j)=h{i,j}';
+            end
         end
-    end
-    varargout{1}=temp;
-    varargout{2}=t;
-    return
-end
-
-% --------------- PLOT ---------------
-%Defining axis limits
-maxOutput=max(cellfun(@max,h),[],2);
-minOutput=min(cellfun(@min,h),[],2);
-deltaOutput=0.1*(maxOutput-minOutput);
-orderMagnitude=floor(log10(deltaOutput))-1;
-
-heightAxis=round(deltaOutput.*10.^(-orderMagnitude)).*10.^orderMagnitude;
-minOutputAxis=minOutput-heightAxis/2;
-maxOutputAxis=maxOutput+heightAxis/2;
-
-minOutputAxis(minOutput>0&minOutputAxis<=0)=0;
-maxOutputAxis(maxOutput<0&maxOutputAxis>=0)=0;
-%Generating figure to plot
-graphic=stepplot(ss(zeros(1,1),zeros(1,sys.m),zeros(sys.p,1),zeros(sys.p,sys.m)),[-2,-1]);
-opt=getoptions(graphic);
-opt.Title.String='Impulse Response';
-setoptions(graphic,opt);
-
-fig_handle=gcf;
-% set random color if figure is not empty
-if isempty(options)
-    %  if ~isempty(get(fig_handle, 'Children'))
-    %      c=rand(3,1); c=c/norm(c);
-    %      options = {'Color', c};
-    %  end
-end
-
-for jIn=1:sys.m
-    for iOut=1:sys.p
-        ax=fig_handle.Children(sys.p*sys.m+1-(jIn-1)*sys.p-(iOut-1));
-        ax.NextPlot='add';
         
-        plot(ax,t, h{iOut,jIn}, options{:});
-        set(ax, 'XLim', [0 max(t)], 'YLim', [minOutputAxis(iOut),maxOutputAxis(iOut)]);
+
+        %varargout{1}=temp;
+        %varargout{2}=t;
+        t=t';
+        return
     end
+    
+    % --------------- PLOT ---------------
+    %Defining axis limits
+    maxOutput=max(cellfun(@max,h),[],2);
+    minOutput=min(cellfun(@min,h),[],2);
+    deltaOutput=0.1*(maxOutput-minOutput);
+    orderMagnitude=floor(log10(deltaOutput))-1;
+    
+    heightAxis=round(deltaOutput.*10.^(-orderMagnitude)).*10.^orderMagnitude;
+    minOutputAxis=minOutput-heightAxis/2;
+    maxOutputAxis=maxOutput+heightAxis/2;
+    
+    minOutputAxis(minOutput>0&minOutputAxis<=0)=0;
+    maxOutputAxis(maxOutput<0&maxOutputAxis>=0)=0;
+    %Generating figure to plot
+    graphic=stepplot(ss(zeros(1,1),zeros(1,sys.m),zeros(sys.p,1),zeros(sys.p,sys.m)),[-2,-1]);
+    opt=getoptions(graphic);
+    opt.Title.String='Impulse Response';
+    setoptions(graphic,opt);
+    
+    fig_handle=gcf;
+    % set random color if figure is not empty
+    if isempty(options)
+        if ~isempty(get(fig_handle, 'Children'))
+            c=rand(3,1); c=c/norm(c);
+            options = {'Color', c};
+        end
+    end
+    
+    for jIn=1:sys.m
+        for iOut=1:sys.p
+            ax=fig_handle.Children(sys.p*sys.m+1-(jIn-1)*sys.p-(iOut-1));
+            ax.NextPlot='add';
+            
+            plot(ax,t, h{iOut,jIn}, options{:});
+            set(ax, 'XLim', [0 max(t)], 'YLim', [minOutputAxis(iOut),maxOutputAxis(iOut)]);
+        end
+    end
+    % avoid output
+    clear temp h t
+    
 end
-% avoid output
-clear h t
-
-
--- src/@sss/step.m
+++ src/@sss/step.m
@@ -1,6 +1,6 @@
-function  [h, t] = step(sys, varargin)
+function  [temp, t] = step(sys, varargin)
 % STEP - Computes and/or plots the step response of a sparse LTI system
-% 
+%
 % Syntax:
 %       STEP(sys)
 %       [h, t] = STEP(sys)
@@ -9,30 +9,29 @@ function  [h, t] = step(sys, varargin)
 %
 % Description:
 %       Computes and/or plots the step response of a sparse LTI system
-% 
-% Input Arguments:       
+%
+% Input Arguments:
 %       *Required Input Arguments:*
 %       -sys: an sss-object containing the LTI system
-%       *Optional Input Arguments:* 
+%       *Optional Input Arguments:*
 %       -t:     vector of time values to plot at
 %       -opts:  plot options, see <a href="matlab:help plot">PLOT</a>
 %
-% Output Arguments:      
+% Output Arguments:
 %       -h, t: vectors containing step response and time vector
 %
 % Examples:
-%       TODO
+%       load cdplayer.mat
+%       sys=sss(A,B,C);
+%       step(sys);
 %
 % See Also:
-%       TODO
-%
-% References:
-%       TODO
+%       sss/residue, sss/impulse
 %
 %------------------------------------------------------------------
-% This file is part of <a href="matlab:docsearch sss">sss</a>, a Sparse State-Space and System Analysis 
+% This file is part of <a href="matlab:docsearch sss">sss</a>, a Sparse State-Space and System Analysis
 % Toolbox developed at the Chair of Automatic Control in collaboration
-% with the Chair of Thermofluid Dynamics, Technische Universitaet Muenchen. 
+% with the Chair of Thermofluid Dynamics, Technische Universitaet Muenchen.
 % For updates and further information please visit <a href="https://www.rt.mw.tum.de/">www.rt.mw.tum.de</a>
 % For any suggestions, submission and/or bug reports, mail us at
 %                   -> <a href="mailto:sssMOR@rt.mw.tum.de">sssMOR@rt.mw.tum.de</a> <-
@@ -59,38 +58,47 @@ else
         assignin('caller', inputname(1), sys);
     end
 end
-
+builtinMATLAB=0;
+if condest(sys.E)>1/(100*eps) %Verify if E is singular
+    warning('Matrix E is close to singular or badly scaled: running the MATLAB built-in function step');
+    builtinMATLAB=1;
+end
+if not(builtinMATLAB)
+    for i=1:numel(p) %Verify if the poles are repeated
+        if p(i)~=0
+            numberOfRepeatedPoles=sum(abs((p-p(i))/p(i))<100*eps);
+        else
+            numberOfRepeatedPoles=sum(abs(p-p(i))<100*eps);
+        end
+        if numberOfRepeatedPoles>1
+            builtinMATLAB=1;
+            warning('System is not diagnoalizable because of repetition of eigenvalues: running the MATLAB built-in function step');
+            break;
+        end
+    end
+end
 options=varargin;
-if nargin>1 && strcmp(class(options{1}), 'double')
+if nargin>1 && isa(options{1}, 'double')
     t=varargin{1};
     options(1)=[];
 end
 
-% is time vector given?
-if exist('t', 'var') && ~isempty(t)
-    % calculate step response
-    h=cellfun(@(x,y) sum(diag(x./p)*transpose(exp(t'*p)-1),1)+y,res,num2cell(sys.D),'UniformOutput',false);
-    h=cellfun(@real,h,'UniformOutput',false);
-    
-else
-    % no, retrieve time values automatically
-    
-    % is decayTime already available?
-    if ~isempty(sys.decayTime)
-        tmax = sys.decayTime;
+if builtinMATLAB
+    if exist('t','var')
+        if nargout>0
+            [temp,t]=step(ss(sys),t);
+        else
+            step(ss(sys),t);
+        end
     else
-        tmax = decayTime(sys);
-        % store system to caller workspace
-        if inputname(1)
-            assignin('caller', inputname(1), sys);
-        end        
+        if nargout>0
+            [temp,t]=step(ss(sys),t);
+        else
+            step(ss(sys),t);
+        end
     end
-    if isnan(tmax)
-        tmax=100;
-    end
-    delta = tmax/999;
-    t = 0:delta:tmax;
-    
+        
+else
     % Change the format of res to work with the following code
     %   original: res = {res1, res2,... resN} where resK = res(p(k)) is the
     %           (possibly matrix-valued) residual to p(k)
@@ -105,21 +113,53 @@ else
             end
         end
     end
-
-    % calculate step response
-    diagCell = cellfun(@(x) diag(x./p),res,'UniformOutput',false);
-    h=cellfun(@(x,y) sum(x*transpose(exp(t'*p)-1),1)+y,diagCell,num2cell(sys.D),'UniformOutput',false);
-    h=cellfun(@real,h,'UniformOutput',false);
-
-    % increase resolution ias long as rel. step size is too large
-    ex=1;
-    while 1
-        refine=0;
-        for iOut=1:sys.p
-            for jIn=1:sys.m
-                m=h{iOut,jIn};
-%                 for k=2:length(m)-1
-%                     if abs(abs(m(k)) - abs(m(k+1)))/(abs(m(k)) + abs(m(k+1))) > 0.5
+    % is time vector given?
+    if exist('t', 'var') && ~isempty(t)
+        % calculate step response
+        diagCell = cellfun(@(x) (x./p),res,'UniformOutput',false);
+        diagCell=cellfun(@nanzero,diagCell,'UniformOutput',false);
+        resZero=cellfun(@(x)x(p==0),res,'UniformOutput',false);
+        if numel(resZero{1})==0
+            resZero=num2cell(zeros(sys.p,sys.m));
+        end
+        h=cellfun(@(x,y,z) sum(x*transpose(exp(t'*p)-1),1)+y+z*t,diagCell,num2cell(sys.D),resZero,'UniformOutput',false);
+        h=cellfun(@real,h,'UniformOutput',false);
+    else
+        % no, retrieve time values automatically
+        
+        % is decayTime already available?
+        if ~isempty(sys.decayTime)
+            tmax = sys.decayTime;
+        else
+            tmax = decayTime(sys);
+            % store system to caller workspace
+            if inputname(1)
+                assignin('caller', inputname(1), sys);
+            end
+        end
+        if isnan(tmax)||isinf(tmax)
+            tmax=100;
+        end
+        delta = tmax/999;
+        t = 0:delta:tmax;
+        
+        % calculate step response
+        diagCell = cellfun(@(x) (x./p),res,'UniformOutput',false);
+        diagCell=cellfun(@nanzero,diagCell,'UniformOutput',false);
+        resZero=cellfun(@(x)x(p==0),res,'UniformOutput',false);
+        if numel(resZero{1})==0
+            resZero=num2cell(zeros(sys.p,sys.m));
+        end
+        h=cellfun(@(x,y,z) sum(x*transpose(exp(t'*p)-1),1)+y+z*t,diagCell,num2cell(sys.D),resZero,'UniformOutput',false);
+        h=cellfun(@real,h,'UniformOutput',false);
+        
+        % increase resolution ias long as rel. step size is too large
+        ex=1;
+        while 1
+            refine=0;
+            for iOut=1:sys.p
+                for jIn=1:sys.m
+                    m=h{iOut,jIn};
                     if any(abs(diff(abs(m)))./((abs(m(1:end-1)))) > 0.5)
                         delta=delta/2;
                         t=0:delta:tmax;
@@ -130,92 +170,103 @@ else
                         refine=1;
                         break
                     end
-%                 end
+                    if refine
+                        break
+                    end
+                end
                 if refine
                     break
                 end
             end
-            if refine
+            if ~refine
+                break
+            end
+            ex=ex+1;
+            if ex==5
                 break
             end
         end
-        if ~refine
-            break
+    end
+    
+    if nargout>0
+        temp=zeros(length(t),sys.p,sys.m);
+        for i=1:sys.p
+            for j=1:sys.m
+                temp(:,i,j)=h{i,j}';
+            end
         end
-        ex=ex+1;
-        if ex==5 
-            break
+        t=t';
+        return
+    end
+    % --------------- PLOT ---------------
+    
+    % set random color if figure is not empty
+    fig_handle=gcf;
+    if isempty(options)
+        if ~isempty(get(fig_handle, 'Children'))
+            c=rand(3,1); c=c/norm(c);
+            options = {'Color', c};
         end
     end
-end
-
-if nargout>0
-    return
-end
-
-% --------------- PLOT ---------------
-
-% set random color if figure is not empty
-fig_handle=gcf;
-if isempty(options)
-    if ~isempty(get(fig_handle, 'Children'))
-        c=rand(3,1); c=c/norm(c);
-        options = {'Color', c};
+    
+    axes_handle=zeros(sys.p,sys.m);
+    
+    maxOutput=max(cellfun(@max,h),[],2);
+    minOutput=min(cellfun(@min,h),[],2);
+    deltaOutput=0.1*(maxOutput-minOutput);
+    orderMagnitude=floor(log10(deltaOutput))-1;
+    
+    heightAxis=round(deltaOutput.*10.^(-orderMagnitude)).*10.^orderMagnitude;
+    minOutputAxis=minOutput-heightAxis/2;
+    maxOutputAxis=maxOutput+heightAxis/2;
+    
+    minOutputAxis(minOutput>0&minOutputAxis<=0)=0;
+    maxOutputAxis(maxOutput<0&maxOutputAxis>=0)=0;
+    
+    
+    for iOut=1:sys.p
+        for jIn=1:sys.m
+            axes_handle(iOut,jIn)=subplot(sys.p,sys.m,(iOut-1)*sys.m+jIn);
+            hold on;
+            
+            if jIn==1 && sys.p>1
+                y_lab=sprintf('To Out(%i)',ceil(iOut/2));
+                ylabel(y_lab,'FontSize',10,'FontName','Helvetica','Color',[0.31,0.31,0.31],...
+                    'FontWeight','normal','FontAngle','normal');
+            end
+            if iOut==1 && sys.m>1
+                x_lab=sprintf('From In(%i)',jIn);
+                title(x_lab,'FontSize',10,'FontName','Helvetica','Color',[0.31,0.31,0.31],...
+                    'FontWeight','normal','FontAngle','normal');
+            end
+            if jIn==1 &&(iOut==sys.p)
+                %do nothing
+            elseif iOut==sys.p
+                set(gca,'ytick',[])
+            elseif jIn==1
+                set(gca,'xtick',[])
+            else
+                set(gca,'xtick',[],'ytick',[])
+            end
+            
+            plot(t, h{iOut,jIn}, options{:});
+            hold on
+            plot([0,max(t)],[h{iOut,jIn}(end),h{iOut,jIn}(end)],':','Color',[0.31 0.31 0.31]);
+            axis([0,max(t),minOutputAxis(iOut),maxOutputAxis(iOut)]);
+        end
     end
+    labelsAxes=axes('Position',[0 0 1 1],'Visible','off');
+    text(0.5,.99,'Step Response','FontName','Helvetica','FontSize',11,'FontWeight','bold','HorizontalAlignment','center','VerticalAlignment','cap');
+    text(0.5,0.01,'Time (seconds)','FontName','Helvetica','FontSize',11,'FontWeight','normal','HorizontalAlignment','center','VerticalAlignment','bottom');
+    yLabel=text(0.01,0.5,'Amplitude','FontName','Helvetica','FontSize',11,'FontWeight','normal','rotation',90);
+    set(yLabel,'HorizontalAlignment','center','VerticalAlignment','top');
+    uistack(labelsAxes,'bottom');
+    z=zoom;
+    setAllowAxesZoom(z,labelsAxes,false);
+    clear h t
 end
-
-axes_handle=zeros(sys.p,sys.m);
-
-maxOutput=max(cellfun(@max,h),[],2);
-minOutput=min(cellfun(@min,h),[],2);
-deltaOutput=0.1*(maxOutput-minOutput);
-orderMagnitude=floor(log10(deltaOutput))-1;
-
-heightAxis=round(deltaOutput.*10.^(-orderMagnitude)).*10.^orderMagnitude;
-minOutputAxis=minOutput-heightAxis/2;
-maxOutputAxis=maxOutput+heightAxis/2;
-
-minOutputAxis(minOutput>0&minOutputAxis<=0)=0;
-maxOutputAxis(maxOutput<0&maxOutputAxis>=0)=0;
-
-
-for iOut=1:sys.p
-    for jIn=1:sys.m
-        axes_handle(iOut,jIn)=subplot(sys.p,sys.m,(iOut-1)*sys.m+jIn);
-        hold on;
-
-        if jIn==1 && sys.p>1
-            y_lab=sprintf('To Out(%i)',ceil(iOut/2));
-            ylabel(y_lab,'FontSize',10,'FontName','Helvetica','Color',[0.31,0.31,0.31],...
-            'FontWeight','normal','FontAngle','normal'); %'FontSmoothing','on'
-        end
-        if iOut==1 && sys.m>1
-            x_lab=sprintf('From In(%i)',jIn);
-            title(x_lab,'FontSize',10,'FontName','Helvetica','Color',[0.31,0.31,0.31],...
-            'FontWeight','normal','FontAngle','normal'); %'FontSmoothing','on',
-        end
-        if jIn==1 &&(iOut==sys.p)
-            %do nothing
-        elseif iOut==sys.p
-            set(gca,'ytick',[])
-        elseif jIn==1
-            set(gca,'xtick',[])
-        else
-            set(gca,'xtick',[],'ytick',[])
-        end
-
-        plot(t, h{iOut,jIn}, options{:});
-        hold on
-        plot([0,max(t)],[h{iOut,jIn}(end),h{iOut,jIn}(end)],':','Color',[0.31 0.31 0.31]);
-        axis([0,max(t),minOutputAxis(iOut),maxOutputAxis(iOut)]);
-    end
 end
-labelsAxes=axes('Position',[0 0 1 1],'Visible','off');
-text(0.5,.99,'Step Response','FontName','Helvetica','FontSize',11,'FontWeight','bold','HorizontalAlignment','center','VerticalAlignment','cap');
-text(0.5,0.01,'Time (seconds)','FontName','Helvetica','FontSize',11,'FontWeight','normal','HorizontalAlignment','center','VerticalAlignment','bottom');
-h=text(0.01,0.5,'Amplitude','FontName','Helvetica','FontSize',11,'FontWeight','normal','rotation',90);
-set(h,'HorizontalAlignment','center','VerticalAlignment','top');
-uistack(labelsAxes,'bottom');
-z=zoom;
-setAllowAxesZoom(z,labelsAxes,false);
-clear h t
+function x = nanzero(x)
+x(isnan(x)) = 0;
+return
+end
