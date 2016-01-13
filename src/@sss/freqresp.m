8e2624e0c708f99cdbb387dd324b3099c7b6c6d8
Alessandro Castagnotto <a.castagnotto@tum.de>
2016-01-13 11:36:31 +0100

    Merge freqresp from UpdatedBode
    for enhanced bode capability

--- src/@sss/freqresp.m
+++ src/@sss/freqresp.m
@@ -1,22 +1,27 @@
-function [G, omega, sys] = freqresp(varargin)
+function [G, omega] = freqresp(varargin)
 % FREQRESP - Evaluates complex transfer function of LTI systems
 % 
 % Description:
 %       Evaluates complex transfer function of LTI systems. If the vector
 %       of complex frequencies is not passed, then a range of imaginary
-%       frequencies is automatically selected.
+%       frequencies is automatically selected. 
+%       This automatic selection is done through the computation of the
+%       first and second derivatives of the magnitude of the frequency
+%       response. 
+%       Also, one of the steps is the computation of the connection between
+%       inputs and outputs of the system. The value of M(i,j) is one when
+%       the input j is connected to the output i. When it is zero, then a
+%       change in the input j doesn't influence the output i.
 %
 % Syntax:
-%       G = freqresp(sys, s)
-%       G = freqresp(sys, s, opts)
-%       [G, omega] = freqresp(sys)
+%       G = freqresp(sys, w)
+%       G = freqresp(sys, w, Opts)
+%       [G, w] = freqresp(sys)
 %
 % Inputs:
 %       *Required Input Arguments:*
 %       -sys: an sss-object containing the LTI system
-%       -s: vector of complex frequencies
-%       *Optional Input Arguments:*
-%       -opts: Computation options, see <a href="matlab:help freqresp">freqresp</a>
+%       -w: vector of frequencies over the imaginary axis
 %       
 % Outputs:      
 %       -G: vector of complex frequency response values
@@ -45,118 +50,310 @@ function [G, omega, sys] = freqresp(varargin)
 % More Toolbox Info by searching <a href="matlab:docsearch sssMOR">sssMOR</a> in the Matlab Documentation
 %
 %------------------------------------------------------------------
-% Authors:      Stefan Jaensch, Heiko Panzer, Sylvia Cremer, Rudy Eid
+% Authors:      Jorge Luiz Moreira Silva, Stefan Jaensch, Heiko Panzer, Sylvia Cremer, Rudy Eid
 %               Lisa Jeschek, Alessandro Castagnotto
 % Email:        <a href="mailto:sssMOR@rt.mw.tum.de">sssMOR@rt.mw.tum.de</a>
 % Website:      <a href="https://www.rt.mw.tum.de/">www.rt.mw.tum.de</a>
 % Work Adress:  Technische Universitaet Muenchen
-% Last Change:  07 Nov 2015
+% Last Change:  20 Dec 2015
 % Copyright (c) 2015 Chair of Automatic Control, TU Muenchen
 %------------------------------------------------------------------
 
 %% input parsing
 sys= varargin{1};
+nOutputs=sys.p;
+nInputs=sys.m;
+[A,B,C,D,E]=dssdata(sys);
+%Reordering Matrices
+reOrderMatrix=abs(A)+abs(E); %abs is used to guarantee that any terms will be cancelled in this combination of A and E
+reOrder=symrcm(reOrderMatrix);
+reOrderMatrix=reOrderMatrix(reOrder,reOrder);
+sys=sss(A(reOrder,reOrder),B(reOrder,:),C(:,reOrder),D,E(reOrder,reOrder));
+%Verifying relation between Inputs and Outputs
+M=InputOutputRelation(sys,reOrderMatrix);
 
 omegaIndex = cellfun(@isfloat,varargin);
 if ~isempty(omegaIndex) && nnz(omegaIndex)
     %frequency vector was specified
     omega = varargin{omegaIndex};
+    %make sure it's a column vector
+    if size(omega,2)>size(omega,1), omega = omega.'; end
+    
     varargin(omegaIndex)=[];
 else
-    %frequency vector will be identified
-    [G,omega,sys] = getFreqRange(sys);
+    if any(any(M))
+    %Finding mininum and maximum frequencies
+    minW=findminW(sys,M);
+    maxW=findmaxW(sys,M);
+    %Compute first points of frequency response
+    qttyPoints=ceil(log(10^(maxW-minW))/log(2))+1;
+    omega=logspace(minW,maxW,qttyPoints)'; %w should be a column according to built-in MATLAB function
+    [firstDerivLog,secondDerivLog,magnitude,resp]=ComputeFreqResp(sys,omega*1i,M);
+    %Refine the frequency response points
+    [G,omega]=FreqRefinement(sys,omega,firstDerivLog,secondDerivLog,magnitude,resp,M);
     return
+    else
+        error('All the inputs of the system are disconnected to all outputs');
+    end
 end
 
 %%  Compute the value of the transfer function at selected freq.
 if (sys.Ts==0) % Convert frequency to either laplace or z variable
-    s = 1i* omega;
+    %{
+    % if omega is a real vector, then make it imaginary for the transfer
+    % function evaluations. If it is purely imaginary, complex or mixed,
+    % than just use the values passed.
+    % NOTE: this is in line with the built-in case. However, if one desired
+    % to evaluate the transfer function ONLY on the real axis, this implementation does not allow it. 
+    % In this case, we recommend evaluating the transfer function manually
+    % or add an imaginary frequency which can be disregarded afterwards
+    %}
+    if isreal(omega)
+        s = 1i* omega;
+    else
+        s = omega;
+    end
 else
-    s = exp(1i* omega*sys.Ts);
-end
-
-for iSys=1:length(varargin)
-    sys= varargin{iSys};
-    m=sys.m; p=sys.p; n=sys.n;
-    [A,B,C,D,E] = dssdata(sys);
-    G=zeros(p,m,length(s));
-    for iShift=1:length(s)
-        G(:,:,iShift) = freqresp_local(A,B,C,D,E, s(iShift),n);
+    if isreal(omega)
+        s = exp(1i* omega*sys.Ts);
+    else
+        s = omega;
     end
 end
+    G=zeros(nOutputs,nInputs,numel(s));
+    [~,~,~,resp]=ComputeFreqResp(sys,s,M);
+    G(1:nOutputs,1:nInputs,:) = resp;
 end
 
-function G = freqresp_local(A,B,C,D,E, s,n)
-% calculate value of transfer function for given vector of frequencies
 
-if isinf(s) || n==0
-    G = D;
-    return
-end
 
-[L,U,k,l,S]=lu(A - s*E, 'vector');
-warning('OFF', 'MATLAB:nearlySingularMatrix')
-b=S\B; b=b(k,:);
-x=L\b;
-x(l,:)=U\x;
-warning('ON', 'MATLAB:nearlySingularMatrix')
-G = -C*x + D;
-end
-
-function [m,omega,sys] = getFreqRange(sys)
-
-% frequency range needs to be chosen
-dc = freqresp(sys,0);    % G(0)=DCgain
-ft = freqresp(sys,inf);  % G(inf)=feedthrough
-
-%determine minimum frequency
-if any(any(isinf(dc))) || any(any(isnan(dc))) % pole at s=0
-    wmin = 0;  dc = ones(size(dc));
-elseif any(any(abs(dc<1e-14)))   % transfer zero at s=0
-    wmin = 0;  dc = ones(size(dc));
-else
-    wmin=0; t = freqresp(sys, -1i*10^wmin);
-    while norm(t-dc)/norm(dc) > 1e-2, 
-        wmin=wmin-1; t = freqresp(sys, 10^wmin);
-    end
-    while norm(t-dc)/norm(dc) < 1e-2
-        wmin=wmin+1; t = freqresp(sys, 10^wmin);
-    end
-    wmin=wmin-1;
-end
-
-%determine maximum frequency
-wmax=0; t = freqresp(sys, -1i*10^wmax);
-while norm(t-ft)/norm(dc) > 1e-6
-    wmax=wmax+1; t = freqresp(sys, 10^wmax);
-end
-while norm(t-ft)/norm(dc) < 1e-6
-    wmax=wmax-1; t = freqresp(sys, 10^wmax);
-end
-wmax=wmax+1;
-
-delta = (wmax-wmin)/19; % initial resolution (insert odd number only!)
-omega = 10.^(wmin:delta:wmax);
-m = freqresp(sys, omega);
-
+function [resp,w]=FreqRefinement(sys,w,firstDerivLog,secondDerivLog,magnitude,resp,M)
+nInputs=sys.m;
+nOutputs=sys.p;
+increment=w(2)/w(1);
+%Enter a loop for the refinement
 while(1)
-    % increase plot density until vertical change per step is below 1%
-    if ~any(any(abs(diff(m,1,3))./abs(m(:,:,1:end-1)) > 0.01))
-        % relative accuracy achieved
-        break
+    %Compute the currentIncrement between all the points
+    currentIncrement=reshape(w(2:end)./w(1:end-1),1,1,numel(w)-1);
+    currentIncrement=repmat(currentIncrement,nOutputs,nInputs,1);
+    %Compute expected value considering just first derivative (make the
+    %plot smooth).
+    Expected=exp(log(magnitude(:,:,1:end-1))+log(currentIncrement).*firstDerivLog(:,:,1:end-1));
+    delta1=abs((Expected-magnitude(:,:,2:end))./(magnitude(:,:,2:end)));
+    delta1(isnan(delta1))=0;
+    %Compute expected value considering first and second derivatives
+    Expected=exp(log(magnitude(:,:,1:end-1))+log(currentIncrement).*firstDerivLog(:,:,1:end-1)+log(currentIncrement).^2.*secondDerivLog(:,:,1:end-1)/2);
+    delta2=abs((Expected-magnitude(:,:,2:end))./(magnitude(:,:,2:end)));
+    delta2(isnan(delta2))=0;
+    %Conditions to refine. If the values get smaller, the final bode will
+    %be more refined.
+    wEvaluated=w(any(any(or(delta1>0.05,delta2>0.01),1),2));
+    if not(numel(wEvaluated))
+        break;
     end
-    if length(omega)>1000 % do not refine above 1000 points
-        break
+    %Compute new incrmeent
+    increment=sqrt(increment);
+    if (increment==1) %it is not possible to increment anymore.
+        break;
     end
-    delta = delta/2;
-    omega = 10.^(wmin:delta:wmax);
-    
-    % calculate new values of frequency response
-    temp=freqresp(sys, omega(2:2:length(omega)));
-    
-    % update array of results (insert new values)
-    mOld = m; m = zeros(size(m,1),size(m,2),length(omega));
-    m(:,:,2:2:length(omega)) = temp; m(:,:,1:2:end) = mOld;
+    wEvaluated=wEvaluated*increment;
+    %Compute the new frequency responses
+    [firstDerivLogComp,secondDerivLogComp,magnitudeComp,respComp]=ComputeFreqResp(sys,wEvaluated*1i,M);
+    %Reorder everything
+    [w,Index]=sort([w;wEvaluated]);
+    firstDerivLog=cat(3,firstDerivLog,firstDerivLogComp);
+    firstDerivLog=firstDerivLog(:,:,Index);
+    secondDerivLog=cat(3,secondDerivLog,secondDerivLogComp);
+    secondDerivLog=secondDerivLog(:,:,Index);
+    magnitude=cat(3,magnitude,magnitudeComp);
+    magnitude=magnitude(:,:,Index);
+    resp=cat(3,resp,respComp);
+    resp=resp(:,:,Index);
+end
 end
 
+function [firstDerivLog,secondDerivLog,magnitude,resp]=ComputeFreqResp(sys,wEval,M)
+[A,B,C,D,E]=dssdata(sys);
+minusA=-A;
+nInputs=sys.m;
+nOutputs=sys.p;
+resp=zeros(nOutputs,nInputs,numel(wEval));
+respp=zeros(nOutputs,nInputs,numel(wEval));
+resppp=zeros(nOutputs,nInputs,numel(wEval));
+M=repmat(not(M),1,1,numel(wEval));
+for i=1:numel(wEval)
+    w=wEval(i);
+    if isinf(w)
+        resp(:,:,i)=D;
+        respp(:,:,i)=nan(size(D));
+        resppp(:,:,i)=nan(size(D));
+    else
+    %Computation of the frequency response for w=wEval(i)
+    [L,U,k,l,S]=lu(minusA+E*w,'vector');
+    warning('OFF', 'MATLAB:nearlySingularMatrix');
+    b=S\B; b=b(k,:);
+    linSolve0=L\b;
+    linSolve0(l,:)=U\linSolve0;
+    resp(:,:,i)=(C*linSolve0)+D;
+    %Computation of the first derivative (Respp) of the frequency response for w=wEval(i)
+    b=S\(E*linSolve0); b=b(k,:);
+    linSolve1=L\b;
+    linSolve1(l,:)=U\linSolve1;
+    respp(:,:,i)=-w*C*linSolve1;
+    %Computation of the second derivative (Resppp) of the frequency response for w=wEval(i)
+    b=S\(E*linSolve1); b=b(k,:);
+    linSolve2=L\b;
+    linSolve2(l,:)=U\linSolve2;
+    warning('ON', 'MATLAB:nearlySingularMatrix');
+    resppp(:,:,i)=2*C*linSolve2;
+    end
+end
+%Computation of the first two derivatives for a log-log of the magnitude plot
+resppp=resppp.*repmat(reshape(wEval,1,1,numel(wEval)),nOutputs,nInputs,1).^2+respp;
+limit=sqrt(eps(0))*10^5; %Limit for change the order of computation (see below)
+magnitude=abs(resp);
+cond=(any(any(magnitude<limit,1),2));
+magSquared=zeros(nOutputs,nInputs,numel(wEval));
+firstDerivLog=zeros(nOutputs,nInputs,numel(wEval));
+secondDerivLog=zeros(nOutputs,nInputs,numel(wEval));
+%Computation of first and second derivatives of conj(Resp).*Resp
+Deriv1=(conj(respp).*resp+conj(resp).*respp);
+Deriv2=2*(real(conj(resppp).*resp)+(conj(respp).*respp));
+
+i=0; %Computation of log derivatives considering only magnitudes>=limit
+w=(cond==i);
+magSquared(:,:,w)=(conj(resp(:,:,w)).*resp(:,:,w));
+firstDerivLog(:,:,w)=(0.5*Deriv1(:,:,w)./magSquared(:,:,w));
+secondDerivLog(:,:,w)=0.5*(Deriv2(:,:,w).*magSquared(:,:,w)-Deriv1(:,:,w).^2)./magSquared(:,:,w)./magSquared(:,:,w); 
+
+i=1; %Computation of log derivatives considering only magnitude<limit
+w=(cond==i);
+firstDerivLog(:,:,w)=(0.5*Deriv1(:,:,w)./magnitude(:,:,w)./magnitude(:,:,w));
+secondDerivLog(:,:,w)=0.5*(Deriv2(:,:,w).*magnitude(:,:,w).*magnitude(:,:,w)-Deriv1(:,:,w).^2)./magnitude(:,:,w)./magnitude(:,:,w)./magnitude(:,:,w)./magnitude(:,:,w); 
+
+%The derivatives of transfer functions of inputs and outputs not connected must be zero
+firstDerivLog(M)=0;
+secondDerivLog(M)=0; 
+%Warning to guarantee that any Derivative will get to infinity. 
+if any(any(any(isinf(firstDerivLog)))) || any(any(any(isinf(secondDerivLog))))
+    warning('The magnitude values of your transfer function got too small and the results might not be precise');
+end
+end
+
+function [M]=InputOutputRelation(sys,S)
+%Function to determine which inputs and outputs are connected through the
+%matrices A and E.
+%The output is a matrix M p x m.
+[~,B,C,~,~]=dssdata(sys);
+C(find(C))=1;
+vec=zeros(sys.m,size(S,1));
+ for k=1:sys.m
+    [i,~]=find(B(:,k));
+    vec(k,i)=1;
+while(1)
+    [~,j]=find(S(i,:));
+    j=unique(j);
+    newVec=(not(vec(k,j)));
+    vec(k,j(newVec))=1;
+    i=j(newVec);
+    if numel(newVec)==0
+        break;
+    end
+end
+ end
+ M=C*vec';
+ M(find(M))=1;
+end
+
+function [minWFinal] = findminW(sys,M)
+[A,B,C,~,E]=dssdata(sys);
+minusA=-A;
+nInputs=size(B,2);
+nOutputs=size(C,1);
+
+normE=norm(E,1);
+normInvA=condest(minusA)/norm(minusA,1);
+minW=1/(normInvA*normE);
+if minW>100*eps
+    minW=minW/100;
+end
+% finding minW
+if minW<=100*eps
+    minW=100*eps;
+end
+
+i=0;
+minW=minW/10;
+CondSuf=not(M);
+Cond=zeros(1,2);
+%minW
+secondDerivLog=0;
+while(1)
+    i=i+1;
+    if i>1
+        minW=minW*10;
+    end
+    Matrix=minusA+E*minW*1i;
+    if any(any(CondSuf==0))
+        Cond=repmat(condest(Matrix),nOutputs,nInputs);
+    end
+    secondDerivLogBefore=secondDerivLog;
+    [~,secondDerivLog,~,~]=ComputeFreqResp(sys,minW*1i,M);
+    Var=inf(nOutputs,nInputs);
+    Var(M&(CondSuf|(Cond>10^-3)))=1e-3;
+    if i>1
+        if Cond>100*eps
+            CondSuf((abs(secondDerivLog)>abs(secondDerivLogBefore))&not(CondSuf))=1;
+        end
+        if any(any(abs(secondDerivLog)>Var))
+            break;
+        end
+    end
+    
+end
+minWFinal=floor(log10(minW(end)*10));
+
+end
+
+function [maxWFinal] = findmaxW(sys,M)
+[A,B,C,~,E]=dssdata(sys);
+minusA=-A;
+nInputs=size(B,2);
+nOutputs=size(C,1);
+normInvE=condest(E)/norm(E,1);
+%Gershgorin circle theorem: one of its implications is that the first norm
+%of a matrix will be greater or equal to its biggest absolute eigenvalue.
+normA=norm(minusA,1);
+maxW=normA*normInvE;
+
+maxW=maxW*10^4;
+
+i=0;
+CondSuf=not(M);%zeros(nInputs,nOutputs);
+Cond=zeros(1,2);
+secondDerivLog=0;
+while(1)
+    i=i+1;
+    if i>1
+        maxW=maxW/10;
+    end
+    Matrix=minusA+E*maxW*1i;
+    if any(any(CondSuf==0))
+        Cond=repmat(condest(Matrix),nOutputs,nInputs);
+    end
+    secondDerivLogBefore=secondDerivLog;
+    [~,secondDerivLog,~,~]=ComputeFreqResp(sys,maxW*1i,M);
+    Var=inf(nOutputs,nInputs);
+    Var(M&(CondSuf|(Cond>10^-3)))=1e-3;
+    if i>1
+        if Cond>100*eps
+            CondSuf((abs(secondDerivLog)>abs(secondDerivLogBefore))&not(CondSuf))=1;
+        end
+        if any(any(abs(secondDerivLog)>Var))
+            break;
+        end
+    end
+    
+end
+maxWFinal=floor(log10(maxW(end)*10));
 end
