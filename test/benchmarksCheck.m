b0e9add87254f8aae71cb980728b5be885b8d719
Rodrigo Mancilla <rodrigo.mancilla@tum.de>
2016-03-14 18:45:30 +0100

    Major benchmarksCheck update for better usability
    The output of benchmarkChecks was updated.
    
    benchmarksCheck is now able to download the benchmarks from https://webdisk.ads.mwn.de
    
    benchmarksCheck now throws an error in case there are problems when
    downloading/copying files from source to destination.
    
    The inputs of benchmarksCheck where changed from optional to
    parameters in order to improve usability.

--- test/benchmarksCheck.m
+++ test/benchmarksCheck.m
@@ -1,4 +1,7 @@
 function benchmarksCheck(varargin)
+
+
+
 %% List of standard benchmarks that should always be in the Benchmarks folder
 defaultBenchmarksList = {...
 	'CDplayer.mat';...
@@ -19,23 +22,24 @@ defaultBenchmarksList = {...
 
 %% Path of local benchmarks folder
 [testFolderPath,~,~] = fileparts(mfilename('fullpath'));
-sssmorFolderPath = fileparts(testFolderPath);
-benchmarksFolderPath = [sssmorFolderPath filesep 'benchmarks' filesep];
+sssFolderPath = fileparts(testFolderPath);
+benchmarksFolderPath = [sssFolderPath filesep 'benchmarks' filesep];
 
-%% Path to the NAS benchmarks folder
-nasFolderPath = [filesep filesep 'nas.ads.mwn.de' filesep 'tumw' filesep 'rtk' filesep 'MORLab' filesep 'Toolbox' filesep '05_benchmark' filesep'];
+%% Path to the Web benchmarks folder
+% nasFolderPath = [filesep filesep 'nas.ads.mwn.de' filesep 'tumw' filesep 'rtk' filesep 'MORLab' filesep 'Toolbox' filesep '05_benchmark' filesep'];
+
+webSource = 'https://webdisk.ads.mwn.de/Handlers/AnonymousDownload.ashx?folder=7d2d07de&path=MORLab%5csssMOR%5cbenchmarks%5c';
 
 %% Parsing
 minInputs = 0;
-maxInputs = 2;
+maxInputs = 6;
 narginchk(minInputs,maxInputs)
 
 p = inputParser;
-checkPath = @(x) any(exist(x,'dir'));
 
-addOptional(p,'destinationPath',benchmarksFolderPath,checkPath);
-addOptional(p,'sourcePath',nasFolderPath,checkPath);
-addOptional(p,'benchmarksList',defaultBenchmarksList,@iscellstr);
+addParameter(p,'sourcePath',webSource,@checkPath); % sourcePath is a web address by default. A non default sourcePath can only be a folder address (no web address)
+addParameter(p,'destinationPath',benchmarksFolderPath,@checkPath);
+addParameter(p,'benchmarksList',defaultBenchmarksList,@iscellstr);
 
 p.FunctionName = 'benchmarks_check';
 
@@ -45,7 +49,18 @@ parse(p,varargin{:});
 sourcePath = p.Results.sourcePath;
 destinationPath = p.Results.destinationPath;
 benchmarksList = p.Results.benchmarksList;
+isWeb = any(strcmp(p.UsingDefaults,'sourcePath'));% Determine if source path is web address (default) or folder address
 
+%% Check for filesep at the end of folder addresses
+if destinationPath(end) ~= filesep
+    destinationPath = [destinationPath filesep];
+end
+
+if ~isWeb % if sourcePath is a folder address (not the default web address)
+    if sourcePath(end) ~= filesep
+        sourcePath = [sourcePath filesep];
+    end
+end
 
 %% Determine which mat-files are already in the local folder
 filesInFolder = what(destinationPath); % struct with files of all types
@@ -54,24 +69,67 @@ matFilesInFolder = filesInFolder.mat; % cell array with names of mat-files
 %% Check if all the standard benchmarks are in the local folder
 nBenchmarks = length(benchmarksList);
 
+fprintf(2,'\nBenchmarks source:\t%s\nBenchmarks destination:\t%s\n\n',sourcePath,destinationPath);
+
 if isempty(matFilesInFolder) % if there are no mat-files in the local folder, then copy all benchmarks
-	for iBenchmarks = 1:nBenchmarks
-		fprintf(2,'Benchmark %s not found. Copying it...\n...from %s\n...to %s\n\n',...
-			benchmarksList{iBenchmarks}, sourcePath, destinationPath);
-		copyfile([sourcePath benchmarksList{iBenchmarks}] , destinationPath);
-	end
+    for iBenchmark = 1:nBenchmarks
+        transferfiles(sourcePath,destinationPath,isWeb,benchmarksList,iBenchmark)
+    end
 else
-	for iBenchmarks = 1:nBenchmarks
-		if ~any(strcmp(p.Results.benchmarksList{iBenchmarks}, matFilesInFolder))
-			fprintf(2,'Benchmark %s not found. Copying it...\n...from %s\n...to %s\n\n',...
-				benchmarksList{iBenchmarks}, sourcePath, destinationPath);
-			copyfile([sourcePath benchmarksList{iBenchmarks}] , destinationPath);
-		end
-	end
+    for iBenchmark = 1:nBenchmarks
+        if ~any(strcmp(benchmarksList{iBenchmark}, matFilesInFolder))
+            transferfiles(sourcePath,destinationPath,isWeb,benchmarksList,iBenchmark)
+        end
+    end
 end
 
+fprintf(2,'-------------------------------------------------------------------------\n');
+fprintf(2,'-------------------------------------------------------------------------\n\n');
+fprintf(2,'Check complete! The following location contains all standard benchmarks:\n\n%s\n\n', destinationPath);
 
 
+end
 
+function transferfiles(sourcePath,destinationPath,isWeb,benchmarksList,iBenchmark)
 
+% options = weboptions(Name,Value);
+
+if isWeb
+    fprintf(2,'Benchmark %s not found. Download in progress...\n',benchmarksList{iBenchmark});
+    
+    try
+        downloadFile = websave(benchmarksList{iBenchmark},[sourcePath benchmarksList{iBenchmark}]);
+    catch
+        fprintf('\n\n');
+        error('Download was unsuccesful. Please check your internet connection!')
+    end
+    
+    movefile(downloadFile,destinationPath)
+    
+    fprintf(2,'%s succesfully downloaded!\n\n',benchmarksList{iBenchmark});
+    
+else
+    fprintf(2,'Benchmark %s not found. Copy in progress...\n',benchmarksList{iBenchmark});
+    
+    try
+        copyfile([sourcePath benchmarksList{iBenchmark}] , destinationPath);
+    catch
+        fprintf('\n\n');
+        error('Copy was unsuccesful. Please check if your source path is correct and accessible!')
+    end
+    
+    fprintf(2,'%s succesfully copied!\n\n',benchmarksList{iBenchmark});
+end
+
+end
+
+function TF = checkPath(path)
+
+TF = any(exist(path,'dir'));
+
+if ~TF
+    error('Couldn''t access/find path. Make sure the path is correct and reachable (e.g. with your file explorer).')
+end
+
+end
 
