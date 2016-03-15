54b1ff072519d13c914a59b2e2a10717d29c8491
Siyang Hu <siyang.hu@tum.de>
2016-03-15 13:15:45 +0100

    added doc for lyapack

---
+++ src/extras/lyapack.m
@@ -0,0 +1,30 @@
+function lyapack
+% LYAPACK - solve large scale problems, closely related to Lyapunov equations
+%
+% Description:
+%       LYAPACK is a MATLAB toolbox (i.e., a set of MATLAB routines) for the 
+%       solution of certain large scale problems in control theory, which are 
+%       closely related to Lyapunov equations. It uses iterative algorithms and
+%       is intended for large, sparse problems.
+%
+%       Amongst other things, it can solve Lyapunov and Riccati equations, and do
+%       model reduction. Even though LYAPACK has been implemented in MATLAB 
+%       rather than programming languages like FORTRAN, C, or JAVA, this does not
+%       mean that LYAPACK is restricted to the solution of "toy problems". 
+%       Several measures, such as the use of global variables for large data 
+%       structures, have been taken to enhance the computational performance of 
+%       LYAPACK routines. To put this into the right perspective, Lyapunov
+%       equations of order larger than 12000 were solved by LYAPACK within few 
+%       hours on a regular workstation. When using standard methods, 
+%       supercomputers are needed to solve problems of this size.
+%
+%------------------------------------------------------------------
+% For more Toolbox Info, have a look at the <a href="http://www.tu-chemnitz.de/sfb393/lyapack/guide.pdf">User Guide</a>
+%------------------------------------------------------------------
+% Authors:      Volker Mehrmann
+% Email:        <a href="mailto:mehrmann@mathematik.tu-chemnitz.de">Volker Mehrmann</a>
+% Website:      <a href="https://www.tu-chemnitz.de/sfb393/lyapack/">www.tu-chemnitz.de/sfb393/lyapack/</a>
+
+
+end
+
