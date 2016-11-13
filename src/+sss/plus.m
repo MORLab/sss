fb4be1a95184c3068950c531b80d897ad1062878
Niklas Kochdumper <niklas.kochdumper@t-online.de>
2016-11-13 20:31:06 +0100

    plus: Adapted in such a way, that ss-objects and ssRed-objects can be added to sss-objects

--- src/+sss/plus.m
+++ src/+sss/plus.m
@@ -41,23 +41,42 @@ function sum = plus(sys1, sys2)
 % Copyright (c) 2015 Chair of Automatic Control, TU Muenchen
 %------------------------------------------------------------------
 
-if sys1.n == 0
+% define system size, because sys.m, sys.n and sys.p are not defined for
+% ss-objects
+sys1n = size(sys1.A,1);
+sys2n = size(sys2.A,1);
+sys1p = size(sys1.B,2);
+sys2p = size(sys2.B,2);
+sys1m = size(sys1.C,1);
+sys2m = size(sys2.C,1);
+
+% change sys.E = [] to sys.E = eye(n)
+if isempty(sys1.E) sys1.E=sparse(eye(sys1n)); end
+if isempty(sys2.E) 
+    if isa(sys2,'sss')
+        sys2.E=sparse(eye(sys2n));
+    else
+        sys2.E=eye(sys2n);
+    end
+end
+
+if sys1n == 0
     sum = sss(sys2.A, sys2.B, sys2.C, sys2.D, sys2.E);
     return
 end
-if sys2.n == 0
+if sys2n == 0
     sum = sss(sys1.A, sys1.B, sys1.C, sys1.D, sys1.E);
     return
 end
-if sys1.p ~= sys2.p
+if sys1p ~= sys2p
     error('sys1 and sys2 must have same number of inputs.')
 end
-if sys1.m ~= sys2.m
+if sys1m ~= sys2m
     error('sys1 and sys2 must have same number of outputs.')
 end
 
-sum = sss([sys1.A sparse(sys1.n,sys2.n); sparse(sys2.n,sys1.n) sys2.A], ...
+sum = sss([sys1.A sparse(sys1n,sys2n); sparse(sys2n,sys1n) sys2.A], ...
           [sys1.B; sys2.B], ...
           [sys1.C, sys2.C], ...
           sys1.D + sys2.D, ...
-          [sys1.E sparse(sys1.n,sys2.n); sparse(sys2.n,sys1.n) sys2.E]);
+          [sys1.E sparse(sys1n,sys2n); sparse(sys2n,sys1n) sys2.E]);
