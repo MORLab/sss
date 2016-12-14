f7777924a21d7bc921c8e4feb0c326777c65ffe0
Niklas Kochdumper <niklas.kochdumper@t-online.de>
2016-12-14 09:16:33 +0100

    testZpk: adapted because zpk has changed
    Because the functionality of zpk was changed, the test had to be adapted. Now the solutions from the sss/zpk function are compared to the ones of the zpk function from the Control System Toolbox

--- test/testScripts/testZpk.m
+++ test/testScripts/testZpk.m
@@ -3,96 +3,55 @@ classdef testZpk < sssTest
    
     methods(Test)
         function testLM(testCase)
-            for i=1:length(testCase.sysCell)
-                sys=testCase.sysCell{i};
+            
+            systemsToTest{1} = loadSss('building');
+            systemsToTest{2} = loadSss('heat-cont');
+            systmesToTest{3} = loadSss('beam');
+            
+            for i=1:length(systemsToTest)
+                sys=systemsToTest{i};
+                sys_ss=ss(sys);
 
                 if ~sys.isDae
                     k=20;
-                    [p,zAll]=zpk(sys,k);
-                    if ~sys.isSiso
-                        p=p{1,1};
-                    end
+                    zpkData=zpk(sys,k,'lm');
+                    zpkData_ss=zpk(sys_ss);
                     
                     %% verification of z
-                    for k=1:sys.m
+                    for j=1:sys.m
                         for l=1:sys.p      
                             if sys.isSiso
-                                z=zAll;
+                                z=cell2mat(zpkData.z);
+                                z_ss=cell2mat(zpkData_ss.z);
                             else
-                                z=zAll{k,l};
+                                z=cell2mat(zpkData(j,l).z);
+                                z_ss=cell2mat(zpkData_ss(j,l).z);
                             end
-
-                            % compare results of eig and eigs
-                            zEig=eig(full([sys.A,sys.B(:,l);sys.C(k,:),sys.D(k,l)]),[full(sys.E),zeros(sys.n,1);zeros(1,sys.n),0]);
-
-                            % remove zeros at infinity
-                            zEig=zEig(abs(real(zEig))<1e6);
-
-                            tbl=table(-abs(z),z);
-                            tbl=sortrows(tbl);
-                            z=tbl.z;
-                            tbl=table(-abs(zEig),zEig);
-                            tbl=sortrows(tbl);
-                            zEig=tbl.zEig;
                             
-                            %disp([z,zEig(1:size(z,1)),abs(z),abs(zEig(1:size(z,1)))]);
-
-                            % remove elements not contained in z from zEig (but all
-                            % elements of z must be in zEig)
-                            j=1;
-                            nRemoved=0;
+                            z_ss = sort(z_ss,'descend');
+                            z_ss = cplxpair(z_ss);
+                            z = sort(z,'descend');
+                            z = cplxpair(z);
                             
-                            while(j<size(z,1))
-                                if  abs(zEig(j,1)-z(j,1))>abs(z(j,1))*1e-4
-                                    zEig(j)=[];
-                                    j=1;
-                                    nRemoved=nRemoved+1;
-                                else
-                                    j=j+1;
-                                end
-                                if j==size(zEig,1)
-                                    break
-                                end
-                            end
-
-                            disp(['Elements not contained in z: ',num2str(nRemoved)]);
-
-                            %remove single complex element (for comparison)
-                            if abs(imag(sum(z)))>1e-12
-                               z(abs(imag(z)-imag(sum(z)))<1e-16)=[];
-                            end
-
-                            verifyLessThanOrEqual(testCase, nRemoved,10);
-                            verification(testCase, z,zEig(1:size(z,1)));
+                            verification(testCase, z,z_ss(1:k));
                         end
                     end
                     
                     
                     %% verification of p
-                    
-                    % compare results of eig and eigs
-                    pEig=eig(full(sys.A),full(sys.E));
-
-                    % remove poles at infinity
-                    pEig=pEig(abs(real(pEig))<1e6);
-
-                    tbl=table(-abs(p),p);
-                    tbl=sortrows(tbl);
-                    p=tbl.p;
-                    tbl=table(-abs(pEig),pEig);
-                    tbl=sortrows(tbl);
-                    pEig=tbl.pEig;
-
-                    %remove single complex element (for comparison)
-                    if abs(imag(sum(p)))>1e-12
-                       p(abs(imag(p)-imag(sum(p)))<1e-16)=[];
+                    if ~sys.isSiso
+                        p=cell2mat(zpkData(1,1).p);
+                        p_ss=cell2mat(zpkData_ss(1,1).p);
+                    else
+                        p=cell2mat(zpkData.p);
+                        p_ss=cell2mat(zpkData_ss.p);
                     end
-                    
-                    % make sure sorting is correct
-                    p=cplxpair(p);
-                    pEig=cplxpair(pEig(1:size(p,1)));
-                    
-                    verification(testCase, p,pEig(1:size(p,1)));
+                    p_ss = sort(p_ss,'descend');
+                    p_ss = cplxpair(p_ss);
+                    p = sort(p,'descend');
+                    p = cplxpair(p);
+                            
+                    verification(testCase, p,p_ss(1:k));            
                 end
             end
         end
