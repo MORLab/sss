055b11165de0a0cd37f9245bda8c6ac9e6d75059
Alessandro Castagnotto <a.castagnotto@tum.de>
2017-01-18 17:53:42 +0100

    impulse - wip
    started to edit impulse so that
    - it does not call step
    - it computes the impulse response using ode
    - it plots the response itself invoking an resppack.timeplot object (ltiplot)
    - it does not return a tf object
    - it works only if all systems have the same input/output dimension
    - the output [y,t] is in the same format as the built-in function (timesteps x outputs x inputs)
    
    so far the function works for
    - SISO model(s)
    
    it is currently being tested for MIMO

--- src/@sss/impulse.m
+++ src/@sss/impulse.m
@@ -12,8 +12,6 @@ function  varargout = impulse(varargin)
 %   [y, t] = IMPULSE(sys,t)
 %   [y, t] = IMPULSE(sys,Tfinal)
 %   [y, t] = IMPULSE(sys,...,Opts)
-%   TF     = IMPULSE(sys,...,struct('tf',true))
-%   [TF,y,t] = IMPULSE(sys,...,struct('tf',true))
 %
 % Description:
 %       Computes and/or plots the impulse response of a sparse LTI system
@@ -30,8 +28,6 @@ function  varargout = impulse(varargin)
 %						[1e-3 / positive float]
 %           -.tolState: Terminate if norm(x-xFinal)/norm(xFinal)<tolState with xFinal = -(A\B);
 %						[1e-3 / positive float]
-%           -.tf: return tf object
-%                       [{0} / 1]
 %           -.ode: ode solver;
 %                       [{'tbd} / 'ode45' / 'ode113' / 'ode15s' / 'ode23'] 
 %           -.tsMin: minimum sample time if no time vector is specified
@@ -71,35 +67,44 @@ function  varargout = impulse(varargin)
 % Email:        <a href="mailto:sss@rt.mw.tum.de">sss@rt.mw.tum.de</a>
 % Website:      <a href="https://www.rt.mw.tum.de/?sss">www.rt.mw.tum.de/?sss</a>
 % Work Adress:  Technische Universitaet Muenchen
-% Last Change:  14 Jun 2016
-% Copyright (c) 2015 Chair of Automatic Control, TU Muenchen
+% Last Change:  18 Jan 2017
+% Copyright (c) 2015,2016 Chair of Automatic Control, TU Muenchen
 %------------------------------------------------------------------
 
     % Make sure the function is used in a correct way before running compts.
+    % Check for
+    % - output variables allowed only if one system is passed
+    % - consistent input/output dimensions throughout models
     nSys = 0;
+    m = []; p = [];
     for iInp = 1:length(varargin)
-        if isa(varargin{iInp},'sss') || isa(varargin{iInp},'ss')  ...
-                || isa(varargin{iInp},'tf') || isa(varargin{iInp},'zpk') ...
-                || isa(varargin{iInp},'frd') || isa(varargin{iInp},'idtf')...
-                || isa(varargin{iInp},'idpoly') || isa(varargin{iInp},'idfrd') ...
-                || isa(varargin{iInp},'idss')
+        argClass = class(varargin{iInp});
+        if any(strcmp(argClass,{'sss','ss', 'tf', 'zpk', 'frd', 'idtf','idpoly',...
+                'idfrd','idss'}))
+            % increase system counter
             nSys = nSys+1;
+            % check input/output size
+            if isempty(m)
+                m = size(varargin{iInp}.B,2); p = size(varargin{iInp}.C,1);
+            elseif m~= size(varargin{iInp}.B,2) || p~= size(varargin{iInp}.C,1)
+                error('sss:impulse:incompatibleInputOutputDimensions',...
+                    'When calling sss/impulse with several LTI models, the number of inputs/outputs has to match.');
+            end
         end
     end
-    if nSys > 1 && nargout
+    if nSys > 1  && nargout
         error('sss:impulse:RequiresSingleModelWithOutputArgs',...
-            'The "impulse" command operates on a single model when used with output arguments.');
-    end
+            'The "impulse" command operates on a single model when used with output arguments.');      
+    end 
 
     %% Parse inputs and options
-    Def.odeset = odeset; % Settings of ODE solver
-    Def.tolOutput = 1e-3; % Terminate if norm(y_-yFinal)/norm(yFinal)<tolOutput with yFinal = C*xFinal+D;
-    Def.tolState = 1e-3; % Terminate if norm(x-xFinal)/norm(xFinal)<tolState with xFinal = -(A\B);
-    Def.tf = 0; % return [y, t] instead of tf object as in bult-in case
-    Def.ode = 'tbd'; %will be determined later depending on the model
-    Def.nMin = 1000; % impulse responses for models with n<nMin are calculated with the built-in Matlab function
-    Def.tsMin = 0; 
-    Def.tLin =0; % linearly spaced t
+    Def.odeset      = odeset; % Settings of ODE solver
+    Def.tolOutput   = 1e-3; % Terminate if norm(y_-yFinal)/norm(yFinal)<tolOutput with yFinal = C*xFinal+D;
+    Def.tolState    = 1e-3; % Terminate if norm(x-xFinal)/norm(xFinal)<tolState with xFinal = -(A\B);
+    Def.ode         = 'tbd'; %will be determined later depending on the model
+    Def.nMin        = 1000; % impulse responses for models with n<nMin are calculated with the built-in Matlab function
+    Def.tsMin       = 0; 
+    Def.tLin        = 0; % linearly spaced t
 
     % create the options structure
     if ~isempty(varargin) && isstruct(varargin{end})
@@ -111,7 +116,6 @@ function  varargout = impulse(varargin)
     else
         Opts = parseOpts(Opts,Def);
     end
-
 
     %% store name of the model
     name = varargin{1}.Name;
@@ -131,202 +135,103 @@ function  varargout = impulse(varargin)
     end
 
     %%  Computation
-    for i = 1:length(varargin)
+    g = cell(1,nSys); tg = cell(1,nSys); % Preallocation
+    
+    for iArgin = 1:length(varargin) % Loop through all systems
+        
         % Set name to input variable name if not specified
-        if isprop(varargin{i},'Name')
-            if isempty(varargin{i}.Name) % Cascaded if is necessary && does not work
-                varargin{i}.Name = inputname(i);
+        if isprop(varargin{iArgin},'Name')
+            if isempty(varargin{iArgin}.Name) % Cascaded if is necessary && does not work
+                varargin{iArgin}.Name = inputname(iArgin);
             end
         end
 
         % Convert sss to frequency response data model
-        if isa(varargin{i},'sss')
-            % get g,t from ode in cell
+        if isa(varargin{iArgin},'sss')        
+            % get g,t from ode
             if ~isempty(t)
-                [varargin{i},tg] = getgt(varargin{i}, t(end),Opts);
+                [g{iArgin},tg{iArgin}] = getgt(varargin{iArgin}, t(end),Opts);
             else
-                [varargin{i},tg] = getgt(varargin{i}, [], Opts);
+                [g{iArgin},tg{iArgin}] = getgt(varargin{iArgin}, [], Opts);
             end
-keyboard %TODO: continue debugging from here
+        elseif isa(varargin{iArgin},'tf') || isa(varargin{iArgin},'ss')
+            % get g,t from built-in impulse
+            [g{iArgin},tg{iArgin}] = impulse(varargin{:},Tfinal);
+        end
+    end
+    
+    %% Plotting or returning outputs
+    if nargout
+        varargout{1} = g{1};
+        varargout{2} = tg{1};
+    else
+        %== Initialize plot as ltiplot (resppack.timeplot)
+        ax = gca;
+        h = ltiplot(ax,'impulse',varargin{1}.InputName,varargin{1}.OutputName,[],cstprefs.tbxprefs);
+        set(h,  'Visible','on')
+        
+        %== Get axes handles to subplots
+        ah = get(gcf,'Children');
+        ah = ah(strcmp('axes',get(ah,'type')));
+        if length(ah)~= m*p
+            error('sss:impulse:wrongNumberOfSubplots','Something went wrong in generating or detecting the subplots for the impulse response');
+        end
+        set(ah, 'XLimMode','auto','YLimMode','auto');
 
-    %         % compute impulse response [g,t]
-    %         for k=1:size(varargin{i},1)
-    %             for j=1:size(varargin{i},2)
-    %                 varargin{i}{k,j}=gradient(varargin{i}{k,j},tg{k,j});
-    %                 tg{k,j}(isnan(varargin{i}{k,j}))=[];
-    %                 varargin{i}{k,j}(isnan(varargin{i}{k,j}))=[];
-    %             end
-    %         end
-
-            % get Ts and Tfinal
-            Ts=Inf;
-            if isempty(t) || isscalar(t)
-                for k=1:size(varargin{i},1)
-                     for j=1:size(varargin{i},2)
-                        Ts=min(min(diff(tg{k,j}),Ts));
-                        if ~isscalar(t)
-                            Tfinal=max(max(tg{k,j}),Tfinal);
-                        end
-                     end
-                end
-                if Ts<Opts.tsMin
-                    warning('Ts changed to Opts.tsMin.');
-                    Ts=Opts.tsMin;
-                elseif Ts<1e-5
-                    warning('Ts is very small. Consider setting Opts.tsMin');
-                end
-            else
-                Ts=min(diff(t));
-                Tfinal=t(end);
-            end
-
-            if nargout==0
-                % compute tf object for plotting
-                for k=1:size(varargin{i},1)
-                     for j=1:size(varargin{i},2)
-                         varargin{i}{k,j}=Ts*interp1(tg{k,j},varargin{i}{k,j},0:Ts:Tfinal);
-                         varargin{i}{k,j}(isnan(varargin{i}{k,j}))=0;
-                     end
-                end
-                varargin{i}=filt(varargin{i},1,Ts);
-            else
-                 % [g,t] or tf object output
-                 if Opts.tf || Opts.tLin
-                     % t from 0 to Tfinal with Ts
-                     t=0:Ts:Tfinal(end);
-                 elseif isempty(t) || isscalar(t)
-                     % add all time points to a single vector
-                     tOut=[];
-                     for k=1:size(tg,1)
-                         for j=1:size(tg,2)
-                            tOut=[tOut;tg{k,j}];
-                         end
-                     end
-                     tOut=sort(tOut);
-
-                     % Tfinal
-                     if isscalar(t)
-                         tOut=tOut(tOut<Tfinal*ones(size(tOut)));
-                     end
-
-                     % keep only elements with abs(u-v)>Ts
-                     t=uniquetol(tOut,Ts/tOut(end));
-                 end
-
-                 % interpolate g at t
-                 if nargout ~= 1 || ~Opts.tf
-                     g=zeros(length(t),size(varargin{1},1),size(varargin{1},2));
-                     for k=1:size(varargin{1},1)
-                         for j=1:size(varargin{1},2)
-                             g(:,k,j)=interp1(tg{k,j},varargin{1}{k,j},t);
-                         end
-                     end
-                 end
-
-                 if Opts.tf
-                    % compute tf object
-                    for k=1:size(varargin{i},1)
-                         for j=1:size(varargin{i},2)
-                             varargin{i}{k,j}=Ts*interp1(tg{k,j},varargin{i}{k,j},t);
-                             varargin{i}{k,j}(isnan(varargin{i}{k,j}))=0;
-                         end
-                    end
-                    TF=filt(varargin{i},1,Ts);
-                 end
-
-                 if nargout==3 && Opts.tf
-                     varargout{1} = TF;
-                     varargout{1}.Name = name;
-                     varargout{2} = g;
-                     varargout{3} = t';
-                 elseif Opts.tf
-                     varargout{1} = TF;
-                     varargout{1}.Name = name;
-                 else
-                     varargout{1}=g;
-                     varargout{2}=t;
-                 end
-                 return;
-            end
-        elseif isa(varargin{i},'tf') && nargout
-            % [g,t] from tf-object -> call built-in
-            if isempty(t)
-                error('Please specifiy t or Tfinal.');
-            end
-            [varargout{1},varargout{2},varargout{3},varargout{4}] = impulse(varargin{:},Tfinal);
-            if ~isscalar(t)
-                % interpolate [g,0:Ts:Tfinal] at t
-                if length(t)==1
-                   t = linspace(0,t(end),length(varargout{2})); 
-                end
-                varargout{1} = interp1(varargout{2},varargout{1},t,'spline');
-                varargout{2} = t;
-
-                % output uniform with built-in
-                if length(size(varargout{1}))==2
-                    varargout{1}=varargout{1}';
-                end
-                if size(varargout{2},1)<size(varargout{2},2)
-                    varargout{2} = varargout{2}';
+        %== plot computed data       
+        for kSys = 1:nSys
+            cnt = 0;
+            for iIn = m:-1:1
+                for jOut = p:-1:1
+                    cnt = cnt+1;
+                    line(tg{kSys},g{kSys}(:,iIn,jOut),...
+                        'Parent',ah(cnt),'DisplayName',varargin{kSys}.Name,...
+                        'Color',pickColor(kSys));
                 end
             end
         end
     end
-
-    %% Plotting with built-in
-    if isempty(t)
-        t=Tfinal;
-    end
-    impulse(varargin{:},t);
-
 end
 
-function [g,tg] = getgt(sys, te, Opts)
-    Opts.tf = false;
-    Opts.htCell = true;
-     
-    sys.d = zeros(size(sys.d));
+%% == AUXILIARY
+
+function [g,te] = getgt(sys, t_, Opts)
+    % In:
+    % sys: system to be simulated
+    % t_:  initial/final time
+    % Opts
     
-    t = te;
-    g = cell(sys.p,sys.m);
-    tg = cell(sys.p,sys.m);
-    for i = 1:sys.p
-        for j=1:sys.m
-            [g{i,j},tg{i,j}] = impulseLocal(truncate(sys,i,j), t, Opts);
-            if size(g{i,j},1)<size(g{i,j},2)
-                g{i,j}=g{i,j}';
-                tg{i,j}=tg{i,j}';
-            end
-        end
-    end
-end
+    % Out:
+    % te: l-dimensional time vector
+    % g: lxpxm array, where p is sys.p and m is sys.m
     
-function [g,te] = impulseLocal(sys, t_, Opts)
+    % Define ODE options
+    optODE  = Opts.odeset;
+    optODE  = odeset(optODE,'Vectorized','on','Jacobian',sys.A);
     
-    x0 = sys.B;
-    optODE = Opts.odeset;
-    [A,B,C,D,E,~] = dssdata(sys);
+    % Define ODE fun
     if ~sys.isDescriptor
-        odeFun = @(t,x) A*x;
+        odeFun = @(t,x) sys.A*x;
     else
         % init solveLse
-        solveLse(E);
+        solveLse(sys.E);
         Opts.reuseLU=true;
 
         % function handle
-        odeFun = @(t,x) solveLse(E,A*x,Opts);
+        odeFun = @(t,x) solveLse(sys.E,sys.A*x,Opts); %more efficient than built-in for sparse models
     end
+    
     if ~isempty(t_)
         tSim = [0,t_(end)];
     else
-        xFinal = -(A\B);
-        yFinal = C*xFinal+D;
         tSim = [0,decayTime(sys)];
-        optODE.OutputFcn = @(t,x,flag) outputFcn(t,x,flag,C,D,xFinal...
-            ,yFinal,Opts.tolOutput,Opts.tolState);
     end
-    te = []; g = te;
-    optODE.Events = @(t,x) eventsFcnT(t,x,C,D);
+    
+    xFinal = zeros(sys.n,1);
+    optODE.OutputFcn = @(t,x,flag) outputFcn(t,x,flag,sys.C,sys.D,xFinal,...
+        Opts.tolOutput,Opts.tolState);    
+    
+    te = []; g = [];
 
     if strcmp(Opts.ode,'tbd')
         solver = chooseSolver(sys);
@@ -334,28 +239,26 @@ function [g,te] = impulseLocal(sys, t_, Opts)
         solver = Opts.ode;
     end
 
-    switch solver
-        case 'ode113'
-            [~,~] = ode113(odeFun,tSim,x0,optODE);
-        case 'ode15s'
-            [~,~] = ode15s(odeFun,tSim,x0,optODE);
-        case 'ode23'
-            [~,~] = ode23(odeFun,tSim,x0,optODE);
-        case 'ode45'
-            [~,~] = ode45(odeFun,tSim,x0,optODE);
-        otherwise
-            error(['The ode solver ' Opts.ode ' is currently not implemented. '  ...
-                'Implemented solvers are: ode113, ode15s, ode23, ode45'])
+    for iIn = 1:sys.m
+        x0 = sys.B(:,iIn); %take respective column of B as initial condition
+        switch solver
+            case 'ode113', ode113(odeFun,tSim,x0,optODE);
+            case 'ode15s', ode15s(odeFun,tSim,x0,optODE);
+            case 'ode23',  ode23(odeFun,tSim,x0,optODE);
+            case 'ode45',  ode45(odeFun,tSim,x0,optODE);
+            otherwise
+                error(['The ode solver ' Opts.ode ' is currently not implemented. '  ...
+                    'Implemented solvers are: ode113, ode15s, ode23, ode45'])
+        end
+        te = cat(3,te,tCurr);
+        g  = cat(3,g, gCurr);
     end
 
-    function [value,isterminal,direction]  = eventsFcnT(t,x,C,D)
-        value = 1;
-        isterminal = 0;
-        direction = [];
-        y_ = full(C*x+D);
-        g = [g, y_'];
-        te = [te, t];
-    end
+%     function [value,isterminal,direction]  = eventsFcnT(t,x,C,D)
+%         value = 1;
+%         isterminal = 0;
+%         direction = [];
+%     end
     function ode = chooseSolver(sys)
         % choose between:
         % ode23, ode45, ode113, ode15s, ode23s, ode23t, and ode23tb
@@ -372,28 +275,63 @@ function [g,te] = impulseLocal(sys, t_, Opts)
         %}
         
         %hard coded. Try to find out if the problem is stiff
-%         ode = 'ode15s'; 
-        ode = 'ode45'; 
+        ode = 'ode15s'; 
+%         ode = 'ode45'; 
     end
 
-end
-
-function status = outputFcn(t,x,flag,C,D,xFinal,yFinal,tolOutput,tolState)
-    status = 0;
-    if isempty(x)
-        return
-    end
-
-    x = x(:,end);
-    y_ = full(C*x+D);
-    if ~isempty(xFinal)
-        y_ = full(C*x+D);
-        if norm(y_-yFinal)/norm(yFinal)<tolOutput || ...
-                norm(x-xFinal)/norm(xFinal)<tolState
-            status = 1;
+    function status = outputFcn(t,x,flag,C,D,xFinal,tolOutput,tolState)
+        status = 0; %initialized as not converged
+        if isempty(x)
+            return
+        end
+        
+        % Generate the current output
+        y_  = full(C*x);
+        
+        %store only the solution (instead of the whole x vector
+        
+        % tCurr: a lx1 array of time instances
+        % gCurr: a lxp array of outputs
+        if strcmp(flag,'init')
+            gCurr  = y_.';
+            tCurr  = t(1);
+        else
+            gCurr = [gCurr; y_.']; 
+            tCurr = [tCurr; t.'];
+        end
+    
+        % Check for convergence to the final solution
+        if ~isempty(xFinal)
+            yFinal = C*xFinal;
+            if norm(y_(:,end)-yFinal)/norm(yFinal)<tolOutput || ...
+                    norm(x(:,end)-xFinal)/norm(xFinal)<tolState
+                status = 1;
+            end
         end
     end
+
 end
+
+function c = pickColor(counter)
+    % Trivial function to loop through plot colors
+    
+    %   Default colors
+    C= [0    0.4470    0.7410
+    0.8500    0.3250    0.0980
+    0.9290    0.6940    0.1250
+    0.4940    0.1840    0.5560
+    0.4660    0.6740    0.1880
+    0.3010    0.7450    0.9330
+    0.6350    0.0780    0.1840];
+    
+    nC = size(C,1); %number of colors
+    
+    % the actual piece of magic
+    currC = counter - nC*floor((counter-1)/nC);    
+    c = C(currC,:);
+end
+
+
 
 
 
