4e2f91d44180572021b57e9216a0cee83157338f
Niklas Kochdumper <niklas.kochdumper@t-online.de>
2017-01-24 23:59:53 +0100

    loadBenchmarks: Added benchmark "iss" to the standard benchmarks that are tested

--- test/testScripts/loadBenchmarks.m
+++ test/testScripts/loadBenchmarks.m
@@ -11,7 +11,7 @@ Def.cond = 'good'; % condition of benchmarks: 'good','bad','all'
                    % 'good': all benchmarks that are not 'bad'
 Def.minSize = 0; % test benchmarks with sys.n >= minSize
 Def.maxSize = 400; % test benchmarks with sys.n <= minSize
-Def.number = 3; % choose maximum number of tested benchmarks
+Def.number = 5; % choose maximum number of tested benchmarks
 Def.dae = 'all'; % 'all', 'withoutDae', 'onlyDae'
 
 % create the options structure
@@ -60,8 +60,8 @@ for i=1:length(files)
             switch(Opts.cond)
                 case 'good'
                      if ~any(strcmp(files{i},badBenchmarks))
-                        if strcmp(files{i},'CDplayer.mat')
-                           benchmarksSysCell{nLoaded}=sys(1,[1 2]);
+                        if strcmp(files{i},'iss.mat')
+                           benchmarksSysCell{nLoaded}=sys([1 3],[1 2 3]);
                         else
                            benchmarksSysCell{nLoaded}=sys;
                         end
