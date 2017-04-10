b04d257bcbfa3d434fe43fdc855167c493cfe8f7
Alessandro Castagnotto <a.castagnotto@tum.de>
2017-04-10 02:52:30 +0200

    freqresp - small bugfix

--- src/@sso/freqresp.m
+++ src/@sso/freqresp.m
@@ -1,8 +1,14 @@
 function [G,w] = freqresp(sys,w)
 
 %   very simple version until the full one works..
+
+% make sure w is real
+if ~isreal(w)
+    w = imag(w);
+end
+
 G = zeros(sys.p,sys.m,length(w));
 for iW = 1:length(w);
     G(:,:,iW) = (sys.Cp + 1i*w(iW)*sys.Cv)*...
-                ((1i*w(iW)^2*sys.M+1i*w(iW)*sys.D+sys.K)\sys.B);
+                (((1i*w(iW))^2*sys.M + 1i*w(iW)*sys.D + sys.K)\sys.B) + sys.Df;
 end
