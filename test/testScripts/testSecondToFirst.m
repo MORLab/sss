d6ca28155f0b32cc84af7306ebd0adda38bc5111
Alessandro Castagnotto <a.castagnotto@tum.de>
2017-04-13 15:40:45 +0200

    testSecondToFirst - avoiding using loadSss (bugfix: sss does not work with Opts as loading of second-order models will be implemented in sso class)

--- test/testScripts/testSecondToFirst.m
+++ test/testScripts/testSecondToFirst.m
@@ -3,33 +3,34 @@ classdef testSecondToFirst < sssTest
     methods (Test)  
         function testDifferentOptions(testCase)
             % test the differnt options for the F-matrix
+            load('LF10');
             
             % option 'I'
             Opts.transf2nd = 'I';
-            sys_I = sss('LF10',Opts);
+            sys_I = second2first(M,D,K,B,C,[],Opts);
             
             % option 'K'
             Opts.transf2nd = 'K';
-            sys_K = sss('LF10',Opts);
+            sys_K = second2first(M,D,K,B,C,[],Opts);
             
             % option '-K'
             Opts.transf2nd = '-K';
-            sys_minK = sss('LF10',Opts);
+            sys_minK = second2first(M,D,K,B,C,[],Opts);
             
             % optoin 'alpha'
             Opts.transf2nd = 'alpha';
-            sys_alpha = sss('LF10',Opts);
+            sys_alpha = second2first(M,D,K,B,C,[],Opts);
             
             % option scalar value
             Opts.transf2nd = 5;
-            sys_scalar = sss('LF10',Opts);
+            sys_scalar = second2first(M,D,K,B,C,[],Opts);
             
             % option user defined matrix
             F = rand(18);
             F = (F+F');
             F = F + 20*eye(18);
             Opts.transf2nd = F;
-            sys_matrix = sss('LF10',Opts);
+            sys_matrix = second2first(M,D,K,B,C,[],Opts);
             
             % create bode-plots
             [mag_I,phase_I,omega] = bode(sys_I);
@@ -40,25 +41,25 @@ classdef testSecondToFirst < sssTest
             [mag_matrix,phase_matrix] = bode(sys_matrix,omega);
             
             % check if the bode-plots are equal
-            verifyEqual(testCase,mag_I,mag_K,'AbsTol',1e-8, ...
+            verifyEqual(testCase,mag_I,mag_K,'RelTol',1e-8, ...
                 'Verification failed for option "K" (magnitude)!');
-            verifyEqual(testCase,phase_I,phase_K,'AbsTol',1e-8, ...
+            verifyEqual(testCase,phase_I,phase_K,'RelTol',1e-8, ...
                 'Verification failed for option "K" (phase)!');
-            verifyEqual(testCase,mag_I,mag_minK,'AbsTol',1e-8, ...
+            verifyEqual(testCase,mag_I,mag_minK,'RelTol',1e-8, ...
                 'Verification failed for option "-K" (magnitude)!');
-            verifyEqual(testCase,phase_I,phase_minK,'AbsTol',1e-8, ...
+            verifyEqual(testCase,phase_I,phase_minK,'RelTol',1e-8, ...
                 'Verification failed for option "-K" (phase)!');
-            verifyEqual(testCase,mag_I,mag_alpha,'AbsTol',1e-8, ...
+            verifyEqual(testCase,mag_I,mag_alpha,'RelTol',1e-8, ...
                 'Verification failed for option "alpha" (magnitude)!');
-            verifyEqual(testCase,phase_I,phase_alpha,'AbsTol',1e-8, ...
+            verifyEqual(testCase,phase_I,phase_alpha,'RelTol',1e-8, ...
                 'Verification failed for option "alpha" (phase)!');
-            verifyEqual(testCase,mag_I,mag_scalar,'AbsTol',1e-8, ...
+            verifyEqual(testCase,mag_I,mag_scalar,'RelTol',1e-8, ...
                 'Verification failed for option "scalar" (magnitude)!');
-            verifyEqual(testCase,phase_I,phase_scalar,'AbsTol',1e-8, ...
+            verifyEqual(testCase,phase_I,phase_scalar,'RelTol',1e-8, ...
                 'Verification failed for option "scalar" (phase)!');
-            verifyEqual(testCase,mag_I,mag_matrix,'AbsTol',1e-8, ...
+            verifyEqual(testCase,mag_I,mag_matrix,'RelTol',1e-8, ...
                 'Verification failed for option "matrix" (magnitude)!');
-            verifyEqual(testCase,phase_I,phase_matrix,'AbsTol',1e-8, ...
+            verifyEqual(testCase,phase_I,phase_matrix,'RelTol',1e-8, ...
                 'Verification failed for option "matrix" (phase)!');     
         end
         
