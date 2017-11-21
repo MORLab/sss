0e5836919cfcef2d275e830281aec3fef589c083
Paul Heidenreich <paul.heidenreich@tum.de>
2017-11-21 14:39:09 +0100

    lyapchol: integration of crksm in sss/lyapchol
    lyapchol can be called using ‘hammering’, ‘crksm’ and ‘adi’

--- test/testScripts/testLyapchol.m
+++ test/testScripts/testLyapchol.m
@@ -21,34 +21,64 @@ classdef testLyapchol < sssTest
             end
         end
         function testLyapcholE(testCase) 
-            for i=1:2
+            for i=1:3
                 if i<2
                     sys=sss('LF10'); % E is symmetric
+                    Opts.method = 'auto';
                 else
                     sys=sss('SpiralInductorPeec');
                 end
-                [S,R]=lyapchol(sys);               
+                if i == 2
+                    Opts.method='adi';
+                elseif i == 3
+                    Opts.method='crksm';
+                    Opts.lowrank = 1;
+                end    
+                [S,R]=lyapchol(sys,Opts);               
                 verification(testCase, sys, S, R);      
             end
         end
         function testMatrices(testCase) 
             sys  = sss('SpiralInductorPeec');
             
-            [S,R]=lyapchol(sys);  
+            % first test adi-method
+            Opts.method = 'adi';
+            [S,R]=lyapchol(sys,Opts);  
             verification(testCase, sys, S, R);  
-            
+
             % compare to matrix call (should be the same)
             S2 = sssFunc.lyapchol(sys.A,sys.B,sys.E);
             R2 = sssFunc.lyapchol(sys.A',sys.C',sys.E');
             verification(testCase, sys, S2, R2); 
             verifyEqual(testCase,S,S2,'AbsTol',1e-3);
             verifyEqual(testCase,R,R2,'AbsTol',1e-3);
-            
+
             % only 2 matrices (E=I)
             sysI = sys; sysI.E = speye(sysI.n);
             S = sssFunc.lyapchol(sysI.A, sysI.B);
             R = sssFunc.lyapchol(sysI.A',sysI.C');
             verification(testCase, sysI, S, R); 
+            clear S R S2 R2
+            
+            % secomd test crksm-method
+            Opts.method = 'crksm';
+            Opts.lowrank = 1;
+            [S,R]=lyapchol(sys,Opts);  
+             verification(testCase, sys, S, R);  
+             
+              % compare to matrix call (should be the same)
+            S2 = sssFunc.lyapchol(sys.A,sys.B,sys.E,Opts);
+            R2 = sssFunc.lyapchol(sys.A',sys.C',sys.E',Opts);
+            verification(testCase, sys, S2, R2); 
+            verifyEqual(testCase,S,S2,'AbsTol',1e-3);
+            verifyEqual(testCase,R,R2,'AbsTol',1e-3);
+
+            % only 2 matrices (E=I)
+            sysI = sys; sysI.E = speye(sysI.n);
+            S = sssFunc.lyapchol(sysI.A, sysI.B,Opts);
+            R = sssFunc.lyapchol(sysI.A',sysI.C',Opts);
+            verification(testCase, sysI, S, R); 
+           
         end
     end
 end
